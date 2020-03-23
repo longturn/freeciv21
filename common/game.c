freeciv/freeciv

Port caravan bonus style and trade revenue style options from warclient
See hrm #765978

Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

common/game.c
@@ -262,6 +262,7 @@ static void game_defaults(bool keep_ruleset_value)
   game.info.angrycitizen     = GAME_DEFAULT_ANGRYCITIZEN;
   game.info.borders          = GAME_DEFAULT_BORDERS;
   game.calendar.calendar_skip_0 = FALSE;
+  game.info.caravan_bonus_style = GAME_DEFAULT_CARAVAN_BONUS_STYLE;
   game.info.celebratesize    = GAME_DEFAULT_CELEBRATESIZE;
   game.info.citymindist      = GAME_DEFAULT_CITYMINDIST;
   game.info.cooling          = 0;
@@ -308,6 +309,7 @@ static void game_defaults(bool keep_ruleset_value)
   game.info.tech             = GAME_DEFAULT_TECHLEVEL;
   game.info.timeout          = GAME_DEFAULT_TIMEOUT;
   game.info.trademindist     = GAME_DEFAULT_TRADEMINDIST;
+  game.info.trade_revenue_style = GAME_DEFAULT_TRADE_REVENUE_STYLE;
   game.info.trading_city     = GAME_DEFAULT_TRADING_CITY;
   game.info.trading_gold     = GAME_DEFAULT_TRADING_GOLD;
   game.info.trading_tech     = GAME_DEFAULT_TRADING_TECH;

common/traderoutes.c
@@ -323,20 +323,31 @@ int trade_base_between_cities(const struct city *pc1, const struct city *pc2)
 {
   int bonus = 0;
 
-  if (NULL != pc1 && NULL != pc1->tile
-      && NULL != pc2 && NULL != pc2->tile) {
+  if (NULL == pc1 || NULL == pc1->tile || NULL == pc2 || NULL == pc2->tile) {
+    return 0;
+  }
+
+  if (game.info.trade_revenue_style == TRS_CLASSIC) {
+    /* Classic Freeciv */
     int real_dist = real_map_distance(pc1->tile, pc2->tile);
     int weighted_distance
       = ((100 - game.info.trade_world_rel_pct) * real_dist
-         + game.info.trade_world_rel_pct * (real_dist * 40 / MAX(wld.map.xsize, wld.map.ysize))) / 100;
+         + game.info.trade_world_rel_pct
+         * (real_dist * 40 / MAX(wld.map.xsize, wld.map.ysize))) / 100;
 
     bonus = weighted_distance
             + city_size_get(pc1) + city_size_get(pc2);
-
-    bonus = bonus * trade_route_type_trade_pct(cities_trade_route_type(pc1, pc2)) / 100;
-
-    bonus /= 12;
+  } else if (game.info.trade_revenue_style == TRS_SIMPLE) {
+    /* Simple revenue style */
+    bonus = (pc1->citizen_base[O_TRADE] + pc2->citizen_base[O_TRADE] + 4)
+	    * 3;
   }
+
+  bonus = bonus
+          * trade_route_type_trade_pct(cities_trade_route_type(pc1, pc2))
+          / 100;
+
+  bonus /= 12;
 
   return bonus;
 }
@@ -363,6 +374,74 @@ int city_num_trade_routes(const struct city *pcity)
 }
 
 /*********************************************************************//**
+  Comparator used in max_tile_trade.
+**************************************************************************/
+static int best_value(const void *a, const void *b)
+{
+  return *(int *)a < *(int *)b;
+}
+
+/*********************************************************************//**
+  Returns the maximum trade production of the tiles of the city.
+**************************************************************************/
+static int max_tile_trade(const struct city *pcity)
+{
+  int i, total = 0;
+  int radius_sq = city_map_radius_sq_get(pcity);
+  int tile_trade[city_map_tiles(radius_sq)];
+  size_t size = 0;
+  bool is_celebrating = base_city_celebrating(pcity);
+
+  if (pcity->tile == NULL) {
+    return 0;
+  }
+
+  city_map_iterate(radius_sq, cindex, cx, cy) {
+    struct tile *ptile = city_map_to_tile(pcity->tile, radius_sq, cx, cy);
+
+    if (ptile == NULL) {
+      continue;
+    }
+
+    if (is_free_worked_index(cindex)) {
+      total += city_tile_output(pcity, ptile, is_celebrating, O_TRADE);
+      continue;
+    }
+
+    if (!city_can_work_tile(pcity, ptile)) {
+      continue;
+    }
+
+    tile_trade[size++] = city_tile_output(pcity, ptile, is_celebrating,
+                                          O_TRADE);
+  } city_map_iterate_end;
+
+  qsort(tile_trade, size, sizeof(*tile_trade), best_value);
+
+  for (i = 0; i < pcity->size && i < size; i++) {
+    total += tile_trade[i];
+  }
+
+  return total;
+}
+
+/*********************************************************************//**
+  Returns the maximum trade production of a city.
+**************************************************************************/
+static int max_trade_prod(const struct city *pcity)
+{
+  /* Trade tile base */
+  int trade_prod = max_tile_trade(pcity);
+
+  /* Add trade routes values */
+  trade_partners_iterate(pcity, partner) {
+    trade_prod += trade_base_between_cities(pcity, partner);
+  } trade_partners_iterate_end;
+
+  return trade_prod;
+}
+
+/*********************************************************************//**
   Returns the revenue trade bonus - you get this when establishing a
   trade route and also when you simply sell your trade goods at the
   new city.
@@ -377,11 +456,19 @@ int get_caravan_enter_city_trade_bonus(const struct city *pc1,
                                        struct goods_type *pgood,
                                        const bool establish_trade)
 {
-  int tb, bonus;
+  int tb = 0, bonus = 0;
 
-  /* Should this be real_map_distance? */
-  tb = map_distance(pc1->tile, pc2->tile) + 10;
-  tb = (tb * (pc1->surplus[O_TRADE] + pc2->surplus[O_TRADE])) / 24;
+  if (game.info.caravan_bonus_style == CBS_CLASSIC) {
+    /* Should this be real_map_distance? */
+    tb = map_distance(pc1->tile, pc2->tile) + 10;
+    tb = (tb * (pc1->surplus[O_TRADE] + pc2->surplus[O_TRADE])) / 24;
+  } else if (game.info.caravan_bonus_style == CBS_LOGARITHMIC) {
+    /* Logarithmic bonus */
+    bonus = pow(log(real_map_distance(pc1->tile, pc2->tile) + 20
+                + max_trade_prod(pc1) + max_trade_prod(pc2)) * 2, 2);
+    tb = (int)bonus;
+  }
+
   if (pgood != NULL) {
     tb = tb * pgood->onetime_pct / 100;
   }

server/settings.c
@@ -215,6 +215,18 @@ static bool set_enum_value(struct setting *pset, int val);
     }
 
 /************************************************************************//**
+  Caravan bonus style setting names accessor.
+****************************************************************************/
+static const struct sset_val_name *caravanbonusstyle_name(int caravanbonus)
+{
+  switch (caravanbonus) {
+  NAME_CASE(CBS_CLASSIC, "CLASSIC", N_("Classic Freeciv"));
+  NAME_CASE(CBS_LOGARITHMIC, "LOGARITHMIC", N_("Log^2 N style"));
+  }
+  return NULL;
+}
+
+/************************************************************************//**
   Map size definition setting names accessor. This setting has an
   hard-coded depedence in "server/meta.c".
 ****************************************************************************/
@@ -238,6 +250,18 @@ static const struct sset_val_name *topology_name(int topology_bit)
   NAME_CASE(TF_WRAPY, "WRAPY", N_("Wrap North-South"));
   NAME_CASE(TF_ISO, "ISO", N_("Isometric"));
   NAME_CASE(TF_HEX, "HEX", N_("Hexagonal"));
+  }
+  return NULL;
+}
+
+/************************************************************************//**
+  Trade revenue style setting names accessor.
+****************************************************************************/
+static const struct sset_val_name *traderevenuestyle_name(int revenue_style)
+{
+  switch (revenue_style) {
+  NAME_CASE(TRS_CLASSIC, "CLASSIC", N_("Classic Freeciv"));
+  NAME_CASE(TRS_SIMPLE, "SIMPLE", N_("Proportional to tile trade"));
   }
   return NULL;
 }
@@ -2079,7 +2103,29 @@ static struct setting settings[] = {
               "is not allowed."), NULL, NULL,
            GAME_DEFAULT_TRADING_CITY)
 
-  GEN_INT("trademindist", game.info.trademindist,
+  GEN_ENUM("caravan_bonus_style", game.info.caravan_bonus_style,
+           SSET_RULES, SSET_ECONOMICS, SSET_RARE, ALLOW_NONE, ALLOW_BASIC,
+           N_("Caravan bonus style"),
+           N_("The formula for the bonus when a caravan enters a city. "
+              "CLASSIC bonuses are proportional to distance and trade "
+              "of source and destination with multipliers for overseas and "
+              "international destinations. LOGARITHMIC bonuses are "
+              "proportional to log^2(distance + trade)."),
+           NULL, NULL, NULL, caravanbonusstyle_name,
+           GAME_DEFAULT_CARAVAN_BONUS_STYLE)
+
+  GEN_ENUM("trade_revenue_style", game.info.trade_revenue_style,
+          SSET_RULES, SSET_ECONOMICS, SSET_RARE, ALLOW_NONE, ALLOW_BASIC,
+          N_("Trade revenue style"),
+          N_("The formula for the trade a city receives from a traderoute. "
+             "CLASSIC revenues depend on distance and trade with "
+             "multipliers for overseas and international routes. "
+             "SIMPLE revenues are proportional to the average trade of the "
+             "two cities."),
+	  NULL, NULL, NULL, traderevenuestyle_name,
+          GAME_DEFAULT_TRADE_REVENUE_STYLE)
+
+    GEN_INT("trademindist", game.info.trademindist,
           SSET_RULES, SSET_ECONOMICS, SSET_RARE, ALLOW_NONE, ALLOW_BASIC,
           N_("Minimum distance for trade routes"),
           N_("In order for two cities in the same civilization to establish "

longturn/freeciv21

common/game.c
@@ -284,6 +284,7 @@ static void game_defaults(void)
   game.info.angrycitizen     = GAME_DEFAULT_ANGRYCITIZEN;
   game.info.borders          = GAME_DEFAULT_BORDERS;
   game.info.calendar_skip_0  = FALSE;
+  game.server.caravan_bonus_style = GAME_DEFAULT_CARAVAN_BONUS_STYLE;
   game.info.celebratesize    = GAME_DEFAULT_CELEBRATESIZE;
   game.info.citymindist      = GAME_DEFAULT_CITYMINDIST;
   game.info.cooling          = 0;
@@ -329,6 +330,7 @@ static void game_defaults(void)
   game.info.tech             = GAME_DEFAULT_TECHLEVEL;
   game.info.timeout          = GAME_DEFAULT_TIMEOUT;
   game.info.trademindist     = GAME_DEFAULT_TRADEMINDIST;
+  game.server.trade_revenue_style = GAME_DEFAULT_TRADE_REVENUE_STYLE;
   game.info.trading_city     = GAME_DEFAULT_TRADING_CITY;
   game.info.trading_gold     = GAME_DEFAULT_TRADING_GOLD;
   game.info.trading_tech     = GAME_DEFAULT_TRADING_TECH;

common/traderoutes.c
@@ -330,16 +330,25 @@ int trade_between_cities(const struct city *pc1, const struct city *pc2)
 {
   int bonus = 0;
 
-  if (NULL != pc1 && NULL != pc1->tile
-      && NULL != pc2 && NULL != pc2->tile) {
+  if (NULL == pc1 || NULL == pc1->tile || NULL == pc2 || NULL == pc2->tile) {
+    return 0;
+  }
 
+  if (game.server.trade_revenue_style == TRS_CLASSIC) {
+    /* Classic Freeciv */
     bonus = real_map_distance(pc1->tile, pc2->tile)
             + city_size_get(pc1) + city_size_get(pc2);
-
-    bonus = bonus * trade_route_type_trade_pct(cities_trade_route_type(pc1, pc2)) / 100;
-
-    bonus /= 12;
+  } else if (game.server.trade_revenue_style == TRS_SIMPLE) {
+    /* Simple revenue style */
+    bonus = (pc1->citizen_base[O_TRADE] + pc2->citizen_base[O_TRADE] + 4)
+	    * 3;
   }
+
+  bonus = bonus
+          * trade_route_type_trade_pct(cities_trade_route_type(pc1, pc2))
+          / 100;
+
+  bonus /= 12;
 
   return bonus;
 }
@@ -361,6 +370,74 @@ int city_num_trade_routes(const struct city *pcity)
 }
 
 /**************************************************************************
+  Comparator used in max_tile_trade.
+**************************************************************************/
+static int best_value(const void *a, const void *b)
+{
+  return *(int *)a < *(int *)b;
+}
+
+/*********************************************************************//**
+  Returns the maximum trade production of the tiles of the city.
+**************************************************************************/
+static int max_tile_trade(const struct city *pcity)
+{
+  int i, total = 0;
+  int radius_sq = city_map_radius_sq_get(pcity);
+  int tile_trade[city_map_tiles(radius_sq)];
+  size_t size = 0;
+  bool is_celebrating = base_city_celebrating(pcity);
+
+  if (pcity->tile == NULL) {
+    return 0;
+  }
+
+  city_map_iterate(radius_sq, cindex, cx, cy) {
+    struct tile *ptile = city_map_to_tile(pcity->tile, radius_sq, cx, cy);
+
+    if (ptile == NULL) {
+      continue;
+    }
+
+    if (is_free_worked_index(cindex)) {
+      total += city_tile_output(pcity, ptile, is_celebrating, O_TRADE);
+      continue;
+    }
+
+    if (!city_can_work_tile(pcity, ptile)) {
+      continue;
+    }
+
+    tile_trade[size++] = city_tile_output(pcity, ptile, is_celebrating,
+                                          O_TRADE);
+  } city_map_iterate_end;
+
+  qsort(tile_trade, size, sizeof(*tile_trade), best_value);
+
+  for (i = 0; i < pcity->size && i < size; i++) {
+    total += tile_trade[i];
+  }
+
+  return total;
+}
+
+/*********************************************************************//**
+  Returns the maximum trade production of a city.
+**************************************************************************/
+static int max_trade_prod(const struct city *pcity)
+{
+  /* Trade tile base */
+  int trade_prod = max_tile_trade(pcity);
+
+  /* Add trade routes values */
+  trade_routes_iterate(pcity, partner) {
+    trade_prod += trade_between_cities(pcity, partner);
+  } trade_routes_iterate_end;
+
+  return trade_prod;
+}
+
+/*********************************************************************//**
   Returns the revenue trade bonus - you get this when establishing a
   trade route and also when you simply sell your trade goods at the
   new city.
@@ -372,16 +449,19 @@ int get_caravan_enter_city_trade_bonus(const struct city *pc1,
                                        const struct city *pc2,
                                        const bool establish_trade)
 {
-  int tb, bonus;
+  int tb = 0, bonus = 0;
 
-  /* Should this be real_map_distance? */
-  tb = map_distance(pc1->tile, pc2->tile) + 10;
-  tb = (tb * (pc1->surplus[O_TRADE] + pc2->surplus[O_TRADE])) / 24;
+  if (game.server.caravan_bonus_style == CBS_CLASSIC) {
+    /* Should this be real_map_distance? */
+    tb = map_distance(pc1->tile, pc2->tile) + 10;
+    tb = (tb * (pc1->surplus[O_TRADE] + pc2->surplus[O_TRADE])) / 24;
+  } else if (game.server.caravan_bonus_style == CBS_LOGARITHMIC) {
+    /* Logarithmic bonus */
+    bonus = pow(log(real_map_distance(pc1->tile, pc2->tile) + 20
+                + max_trade_prod(pc1) + max_trade_prod(pc2)) * 2, 2);
+    tb = (int)bonus;
+  }
 
-  /*  fudge factor to more closely approximate Civ2 behavior (Civ2 is
-   * really very different -- this just fakes it a little better) */
-  tb *= 3;
-  
   /* Trade_revenue_bonus increases revenue by power of 2 in milimes */
   bonus = get_city_bonus(pc1, EFT_TRADE_REVENUE_BONUS);
   

server/settings.c
@@ -210,7 +210,19 @@ static bool set_enum_value(struct setting *pset, int val);
       return &name;                                                         \
     }
 
-/****************************************************************************
+/************************************************************************//**
+  Caravan bonus style setting names accessor.
+****************************************************************************/
+static const struct sset_val_name *caravanbonusstyle_name(int caravanbonus)
+{
+  switch (caravanbonus) {
+  NAME_CASE(CBS_CLASSIC, "CLASSIC", N_("Classic Freeciv"));
+  NAME_CASE(CBS_LOGARITHMIC, "LOGARITHMIC", N_("Log^2 N style"));
+  }
+  return NULL;
+}
+
+/************************************************************************//**
   Map size definition setting names accessor. This setting has an
   hard-coded depedence in "server/meta.c".
 ****************************************************************************/
@@ -238,7 +250,19 @@ static const struct sset_val_name *topology_name(int topology_bit)
   return NULL;
 }
 
-/****************************************************************************
+/************************************************************************//**
+  Trade revenue style setting names accessor.
+****************************************************************************/
+static const struct sset_val_name *traderevenuestyle_name(int revenue_style)
+{
+  switch (revenue_style) {
+  NAME_CASE(TRS_CLASSIC, "CLASSIC", N_("Classic Freeciv"));
+  NAME_CASE(TRS_SIMPLE, "SIMPLE", N_("Proportional to tile trade"));
+  }
+  return NULL;
+}
+
+/************************************************************************//**
   Generator setting names accessor.
 ****************************************************************************/
 static const struct sset_val_name *generator_name(int generator)
@@ -1985,7 +2009,29 @@ static struct setting settings[] = {
               "is not allowed."), NULL, NULL,
            GAME_DEFAULT_TRADING_CITY)
 
-  GEN_INT("trademindist", game.info.trademindist,
+  GEN_ENUM("caravan_bonus_style", game.server.caravan_bonus_style,
+          SSET_RULES, SSET_ECONOMICS, SSET_RARE, SSET_TO_CLIENT,
+           N_("Caravan bonus style"),
+           N_("The formula for the bonus when a caravan enters a city. "
+              "CLASSIC bonuses are proportional to distance and trade "
+              "of source and destination with multipliers for overseas and "
+              "international destinations. LOGARITHMIC bonuses are "
+              "proportional to log^2(distance + trade)."),
+           NULL, NULL, NULL, caravanbonusstyle_name,
+           GAME_DEFAULT_CARAVAN_BONUS_STYLE)
+
+  GEN_ENUM("trade_revenue_style", game.server.trade_revenue_style,
+          SSET_RULES, SSET_ECONOMICS, SSET_RARE, SSET_TO_CLIENT,
+          N_("Trade revenue style"),
+          N_("The formula for the trade a city receives from a traderoute. "
+             "CLASSIC revenues depend on distance and trade with "
+             "multipliers for overseas and international routes. "
+             "SIMPLE revenues are proportional to the average trade of the "
+             "two cities."),
+	  NULL, NULL, NULL, traderevenuestyle_name,
+          GAME_DEFAULT_TRADE_REVENUE_STYLE)
+
+    GEN_INT("trademindist", game.info.trademindist,
           SSET_RULES, SSET_ECONOMICS, SSET_RARE, SSET_TO_CLIENT,
           N_("Minimum distance for trade routes"),
           N_("In order for two cities in the same civilization to establish "