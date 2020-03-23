andreasrosdal/freeciv

Specialize database.lua to longturn.net

lua/database.lua
@@ -12,172 +12,94 @@
 -- This file is the Freeciv server`s interface to the database backend
 -- when authentication is enabled. See doc/README.fcdb.
 
+-- Lua Base64 License
+
+-- Copyright (c) 2012, Daniel Lindsley
+-- All rights reserved.
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 
+-- * Redistributions of source code must retain the above copyright notice, this
+--   list of conditions and the following disclaimer.
+-- * Redistributions in binary form must reproduce the above copyright notice,
+--   this list of conditions and the following disclaimer in the documentation
+--   and/or other materials provided with the distribution.
+-- * Neither the name of the base64 nor the names of its contributors may be
+--   used to endorse or promote products derived from this software without
+--   specific prior written permission.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+local index_table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+local function to_binary(integer)
+    local remaining = tonumber(integer)
+    local bin_bits = ''
+
+    for i = 7, 0, -1 do
+        local current_power = 2 ^ i
+
+        if remaining >= current_power then
+            bin_bits = bin_bits .. '1'
+            remaining = remaining - current_power
+        else
+            bin_bits = bin_bits .. '0'
+        end
+    end
+
+    return bin_bits
+end
+
+local function from_binary(bin_bits)
+    return tonumber(bin_bits, 2)
+end
+
+local function from_base64(to_decode)
+    local padded = to_decode:gsub("%s", "")
+    local unpadded = padded:gsub("=", "")
+    local bit_pattern = ''
+    local decoded = ''
+
+    for i = 1, string.len(unpadded) do
+        local char = string.sub(to_decode, i, i)
+        local offset, _ = string.find(index_table, char)
+        if offset == nil then
+             error("Invalid character '" .. char .. "' found.")
+        end
+
+        bit_pattern = bit_pattern .. string.sub(to_binary(offset-1), 3)
+    end
+
+    for i = 1, string.len(bit_pattern), 8 do
+        local byte = string.sub(bit_pattern, i, i+7)
+        decoded = decoded .. string.char(from_binary(byte))
+    end
+
+    local padding_length = padded:len()-unpadded:len()
+
+    if (padding_length == 1 or padding_length == 2) then
+        decoded = decoded:sub(1,-2)
+    end
+    return decoded
+end
+
 local dbh = nil
 
--- Machinery for debug logging of options
-local seen_options
-local function options_init()
-  seen_options = {}
+function check_password(hash, plaintext)
+  local c, salt, hash = string.match(hash, 'pbkdf2_sha256$(%d+)$([^$]+)$(.*)')
+  local got = pbkdf2_sha256(plaintext, salt, tonumber(c), 256)
+  return from_base64(hash) == got
 end
-local function option_log(name, val, is_sensitive, source)
-  if not seen_options[name] then
-    seen_options[name] = true
-    if is_sensitive then
-      log.debug('Database option \'%s\': %s', name, source)
-    else
-      log.debug('Database option \'%s\': %s: value \'%s\'', name, source, val)
-    end
-  end
-end
-
--- Get an option from configuration file, falling back to sensible
--- defaults where they exist
-local function get_option(name, is_sensitive)
-  local defaults = {
-    backend    = "sqlite",
-    table_user = "fcdb_auth",
-    table_log  = "fcdb_log"
-  }
-  local val = fcdb.option(name)
-  if val then
-    option_log(name, val, is_sensitive, 'read from file')
-  else
-    val = defaults[name]
-    if val then
-      option_log(name, val, is_sensitive, 'using default')
-    end
-  end
-  if not val then
-    log.error('Database option \'%s\' not specified in configuration file',
-              name)
-  end
-  return val
-end
-
--- connect to a MySQL database (or raise an error)
-local function mysql_connect()
-  if dbh then
-    dbh:close()
-  end
-
-  local sql = ls_mysql.mysql()
-
-  log.verbose('MySQL database version is %s.', ls_mysql._MYSQLVERSION)
-
-  -- Load the database parameters.
-  local database = get_option("database")
-  local user     = get_option("user")
-  local password = get_option("password", true)
-  local host     = get_option("host")
-  local port     = get_option("port")
-
-  dbh = assert(sql:connect(database, user, password, host, port))
-end
-
--- open a SQLite database (or raise an error)
-local function sqlite_connect()
-  if dbh then
-    dbh:close()
-  end
-
-  local sql = ls_sqlite3.sqlite3()
-
-  -- Load the database parameters.
-  local database = get_option("database")
-
-  dbh = assert(sql:connect(database))
-end
-
--- DIRTY: return a string to put in a database query which gets the
--- current time (in seconds since the epoch, UTC).
--- (This should be replaced with Lua os.time() once the script has access
--- to this, see <https://www.hostedredmine.com/issues/657141>.)
-function sql_time()
-  local backend = get_option("backend")
-  if backend == 'mysql' then
-    return 'UNIX_TIMESTAMP()'
-  elseif backend == 'sqlite' then
-    return 'strftime(\'%s\',\'now\')'
-  else
-    log.error('Don\'t know how to do timestamps for database backend \'%s\'', backend)
-    return 'ERROR'
-  end
-end
-
--- Set up tables for an SQLite database.
--- (Since there`s no concept of user rights, we can do this directly from Lua,
--- without needing a separate script like MySQL. The server operator can do
--- "/fcdb lua sqlite_createdb()" from the server prompt.)
-function sqlite_createdb()
-  local query
-
-  if get_option("backend") ~= 'sqlite' then
-    error("'backend' in configuration file must be 'sqlite'")
-  end
-
-  local table_user = get_option("table_user")
-  local table_log  = get_option("table_log")
-
-  if not dbh then
-    error("Missing database connection")
-  end
-
-  query = string.format([[
-CREATE TABLE %s (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(48) default NULL UNIQUE,
-  password VARCHAR(32) default NULL,
-  email VARCHAR default NULL,
-  createtime INTEGER default NULL,
-  accesstime INTEGER default NULL,
-  address VARCHAR default NULL,
-  createaddress VARCHAR default NULL,
-  logincount INTEGER default '0'
-);
-]], table_user)
-  assert(dbh:execute(query))
-
-  query = string.format([[
-CREATE TABLE %s (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(48) default NULL,
-  logintime INTEGER default NULL,
-  address VARCHAR default NULL,
-  succeed TEXT default 'S'
-);]], table_log)
-  assert(dbh:execute(query))
-end
-
--- **************************************************************************
--- For MySQL, the following shapes of tables are expected
--- (scripts/setup_auth_server.sh automates this):
---
--- CREATE TABLE fcdb_auth (
---   id int(11) NOT NULL auto_increment,
---   name varchar(48) default NULL,
---   password varchar(32) default NULL,
---   email varchar(128) default NULL,
---   createtime int(11) default NULL,
---   accesstime int(11) default NULL,
---   address varchar(255) default NULL,
---   createaddress varchar(255) default NULL,
---   logincount int(11) default '0',
---   PRIMARY KEY  (id),
---   UNIQUE KEY name (name)
--- );
---
--- CREATE TABLE fcdb_log (
---   id int(11) NOT NULL auto_increment,
---   name varchar(48) default NULL,
---   logintime int(11) default NULL,
---   address varchar(255) default NULL,
---   succeed enum('S','F') default 'S',
---   PRIMARY KEY  (id)
--- );
---
--- N.B. if the tables are not of this format, then the select, insert,
---      and update syntax in the following functions must be changed.
--- **************************************************************************
 
 -- **************************************************************************
 -- freeciv user auth functions
@@ -185,19 +107,14 @@ end
 
 -- Check if user exists.
 function user_exists(conn)
-  local res     -- result handle
-
-  local table_user = get_option("table_user")
-
   if not dbh then
     error("Missing database connection...")
   end
 
   local username = dbh:escape(auth.get_username(conn))
 
-  query = string.format([[SELECT count(*) FROM %s WHERE name = '%s']],
-                        table_user, username)
-  res = assert(dbh:execute(query))
+  query = string.format([[SELECT count(*) FROM auth_user WHERE username = '%s']], username)
+  local res = assert(dbh:execute(query))
 
   local count = res:fetch()
   res:close()
@@ -207,14 +124,6 @@ end
 
 -- Check user password.
 function user_verify(conn, plaintext)
-  local res     -- result handle
-  local row     -- one row of the sql result
-  local query   -- sql query
-
-  local fields = 'password'
-
-  local table_user = get_option("table_user")
-
   if not dbh then
     error("Missing database connection...")
   end
@@ -222,11 +131,10 @@ function user_verify(conn, plaintext)
   local username = dbh:escape(auth.get_username(conn))
 
   -- get the password for this user
-  query = string.format([[SELECT %s FROM %s WHERE name = '%s']],
-                        fields, table_user, username)
-  res = assert(dbh:execute(query))
+  local query = string.format([[SELECT password FROM auth_users WHERE username = '%s']], username)
+  local res = assert(dbh:execute(query))
 
-  row = res:fetch({}, 'a')
+  local row = res:fetch({}, 'a')
   if not row then
     -- No match
     res:close()
@@ -242,60 +150,15 @@ function user_verify(conn, plaintext)
 
   res:close()
 
-  return row.password == md5sum(plaintext)
+  return check_password(row.password, plaintext)
 end
 
 -- save a user to the database
 function user_save(conn, password)
-  local table_user = get_option("table_user")
-
-  if not dbh then
-    error("Missing database connection...")
-  end
-
-  local username = dbh:escape(auth.get_username(conn))
-  local ipaddr = auth.get_ipaddr(conn)
-
-  -- insert the user
-  --local now = os.time()
-  local query = string.format([[INSERT INTO %s VALUES (NULL, '%s', '%s',
-                                NULL, %s, %s, '%s', '%s', 0)]],
-                              table_user, username, md5sum(password),
-                              sql_time(), sql_time(),
-                              ipaddr, ipaddr)
-  assert(dbh:execute(query))
-
-  user_log(conn, true)
 end
 
 -- log the session
 function user_log(conn, success)
-  local query   -- sql query
-
-  if not dbh then
-    error("Missing database connection...")
-  end
-
-  local table_user = get_option("table_user")
-  local table_log  = get_option("table_log")
-
-  local username = dbh:escape(auth.get_username(conn))
-  local ipaddr = auth.get_ipaddr(conn)
-  local success_str = success and 'S' or 'F'
-
-  -- update user data
-  --local now = os.time()
-  query = string.format([[UPDATE %s SET accesstime = %s, address = '%s',
-                          logincount = logincount + 1
-                          WHERE name = '%s']], table_user, sql_time(),
-                          ipaddr, username)
-  assert(dbh:execute(query))
-
-  -- insert the log row for this user
-  query = string.format([[INSERT INTO %s (name, logintime, address, succeed)
-                          VALUES ('%s', %s, '%s', '%s')]],
-                        table_log, username, sql_time(), ipaddr, success_str)
-  assert(dbh:execute(query))
 end
 
 -- **************************************************************************
@@ -304,23 +167,9 @@ end
 
 -- test and initialise the database connection
 function database_init()
-  options_init()
+  local sql = ls_postgres.postgres()
 
-  local backend = get_option("backend")
-
-  if backend == 'mysql' then
-    log.verbose('Opening MySQL database connection...')
-    return mysql_connect()
-  end
-
-  if backend == 'sqlite' then
-    log.verbose('Opening SQLite database connection...')
-    return sqlite_connect()
-  end
-
-  error(string.format(
-    'Database backend \'%s\' not supported by database.lua',
-    backend))
+  dbh = assert(sql:connect("longturn", "longturn"))
 end
 
 -- free the database connection

longturn/freeciv21

lua/database.lua
@@ -12,172 +12,94 @@
 -- This file is the Freeciv server`s interface to the database backend
 -- when authentication is enabled. See doc/README.fcdb.
 
+-- Lua Base64 License
+
+-- Copyright (c) 2012, Daniel Lindsley
+-- All rights reserved.
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 
+-- * Redistributions of source code must retain the above copyright notice, this
+--   list of conditions and the following disclaimer.
+-- * Redistributions in binary form must reproduce the above copyright notice,
+--   this list of conditions and the following disclaimer in the documentation
+--   and/or other materials provided with the distribution.
+-- * Neither the name of the base64 nor the names of its contributors may be
+--   used to endorse or promote products derived from this software without
+--   specific prior written permission.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+local index_table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+local function to_binary(integer)
+    local remaining = tonumber(integer)
+    local bin_bits = ''
+
+    for i = 7, 0, -1 do
+        local current_power = 2 ^ i
+
+        if remaining >= current_power then
+            bin_bits = bin_bits .. '1'
+            remaining = remaining - current_power
+        else
+            bin_bits = bin_bits .. '0'
+        end
+    end
+
+    return bin_bits
+end
+
+local function from_binary(bin_bits)
+    return tonumber(bin_bits, 2)
+end
+
+local function from_base64(to_decode)
+    local padded = to_decode:gsub("%s", "")
+    local unpadded = padded:gsub("=", "")
+    local bit_pattern = ''
+    local decoded = ''
+
+    for i = 1, string.len(unpadded) do
+        local char = string.sub(to_decode, i, i)
+        local offset, _ = string.find(index_table, char)
+        if offset == nil then
+             error("Invalid character '" .. char .. "' found.")
+        end
+
+        bit_pattern = bit_pattern .. string.sub(to_binary(offset-1), 3)
+    end
+
+    for i = 1, string.len(bit_pattern), 8 do
+        local byte = string.sub(bit_pattern, i, i+7)
+        decoded = decoded .. string.char(from_binary(byte))
+    end
+
+    local padding_length = padded:len()-unpadded:len()
+
+    if (padding_length == 1 or padding_length == 2) then
+        decoded = decoded:sub(1,-2)
+    end
+    return decoded
+end
+
 local dbh = nil
 
--- Machinery for debug logging of options
-local seen_options
-local function options_init()
-  seen_options = {}
+function check_password(hash, plaintext)
+  local c, salt, hash = string.match(hash, 'pbkdf2_sha256$(%d+)$([^$]+)$(.*)')
+  local got = pbkdf2_sha256(plaintext, salt, tonumber(c), 256)
+  return from_base64(hash) == got
 end
-local function option_log(name, val, is_sensitive, source)
-  if not seen_options[name] then
-    seen_options[name] = true
-    if is_sensitive then
-      log.debug('Database option \'%s\': %s', name, source)
-    else
-      log.debug('Database option \'%s\': %s: value \'%s\'', name, source, val)
-    end
-  end
-end
-
--- Get an option from configuration file, falling back to sensible
--- defaults where they exist
-local function get_option(name, is_sensitive)
-  local defaults = {
-    backend    = "sqlite",
-    table_user = "fcdb_auth",
-    table_log  = "fcdb_log"
-  }
-  local val = fcdb.option(name)
-  if val then
-    option_log(name, val, is_sensitive, 'read from file')
-  else
-    val = defaults[name]
-    if val then
-      option_log(name, val, is_sensitive, 'using default')
-    end
-  end
-  if not val then
-    log.error('Database option \'%s\' not specified in configuration file',
-              name)
-  end
-  return val
-end
-
--- connect to a MySQL database (or raise an error)
-local function mysql_connect()
-  if dbh then
-    dbh:close()
-  end
-
-  local sql = ls_mysql.mysql()
-
-  log.verbose('MySQL database version is %s.', ls_mysql._MYSQLVERSION)
-
-  -- Load the database parameters.
-  local database = get_option("database")
-  local user     = get_option("user")
-  local password = get_option("password", true)
-  local host     = get_option("host")
-  local port     = get_option("port")
-
-  dbh = assert(sql:connect(database, user, password, host, port))
-end
-
--- open a SQLite database (or raise an error)
-local function sqlite_connect()
-  if dbh then
-    dbh:close()
-  end
-
-  local sql = ls_sqlite3.sqlite3()
-
-  -- Load the database parameters.
-  local database = get_option("database")
-
-  dbh = assert(sql:connect(database))
-end
-
--- DIRTY: return a string to put in a database query which gets the
--- current time (in seconds since the epoch, UTC).
--- (This should be replaced with Lua os.time() once the script has access
--- to this, see <https://www.hostedredmine.com/issues/657141>.)
-function sql_time()
-  local backend = get_option("backend")
-  if backend == 'mysql' then
-    return 'UNIX_TIMESTAMP()'
-  elseif backend == 'sqlite' then
-    return 'strftime(\'%s\',\'now\')'
-  else
-    log.error('Don\'t know how to do timestamps for database backend \'%s\'', backend)
-    return 'ERROR'
-  end
-end
-
--- Set up tables for an SQLite database.
--- (Since there`s no concept of user rights, we can do this directly from Lua,
--- without needing a separate script like MySQL. The server operator can do
--- "/fcdb lua sqlite_createdb()" from the server prompt.)
-function sqlite_createdb()
-  local query
-
-  if get_option("backend") ~= 'sqlite' then
-    error("'backend' in configuration file must be 'sqlite'")
-  end
-
-  local table_user = get_option("table_user")
-  local table_log  = get_option("table_log")
-
-  if not dbh then
-    error("Missing database connection")
-  end
-
-  query = string.format([[
-CREATE TABLE %s (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(48) default NULL UNIQUE,
-  password VARCHAR(32) default NULL,
-  email VARCHAR default NULL,
-  createtime INTEGER default NULL,
-  accesstime INTEGER default NULL,
-  address VARCHAR default NULL,
-  createaddress VARCHAR default NULL,
-  logincount INTEGER default '0'
-);
-]], table_user)
-  assert(dbh:execute(query))
-
-  query = string.format([[
-CREATE TABLE %s (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(48) default NULL,
-  logintime INTEGER default NULL,
-  address VARCHAR default NULL,
-  succeed TEXT default 'S'
-);]], table_log)
-  assert(dbh:execute(query))
-end
-
--- **************************************************************************
--- For MySQL, the following shapes of tables are expected
--- (scripts/setup_auth_server.sh automates this):
---
--- CREATE TABLE fcdb_auth (
---   id int(11) NOT NULL auto_increment,
---   name varchar(48) default NULL,
---   password varchar(32) default NULL,
---   email varchar(128) default NULL,
---   createtime int(11) default NULL,
---   accesstime int(11) default NULL,
---   address varchar(255) default NULL,
---   createaddress varchar(255) default NULL,
---   logincount int(11) default '0',
---   PRIMARY KEY  (id),
---   UNIQUE KEY name (name)
--- );
---
--- CREATE TABLE fcdb_log (
---   id int(11) NOT NULL auto_increment,
---   name varchar(48) default NULL,
---   logintime int(11) default NULL,
---   address varchar(255) default NULL,
---   succeed enum('S','F') default 'S',
---   PRIMARY KEY  (id)
--- );
---
--- N.B. if the tables are not of this format, then the select, insert,
---      and update syntax in the following functions must be changed.
--- **************************************************************************
 
 -- **************************************************************************
 -- freeciv user auth functions
@@ -185,36 +107,23 @@ end
 
 -- Check if user exists.
 function user_exists(conn)
-  local res     -- result handle
-
-  local table_user = get_option("table_user")
-
   if not dbh then
     error("Missing database connection...")
   end
 
   local username = dbh:escape(auth.get_username(conn))
 
-  query = string.format([[SELECT count(*) FROM %s WHERE name = '%s']],
-                        table_user, username)
-  res = assert(dbh:execute(query))
+  local query = string.format([[SELECT count(*) FROM auth_user WHERE username = '%s']], username)
+  local res = assert(dbh:execute(query))
 
-  local count = res:fetch()
+  local row = res:fetch()
   res:close()
 
-  return count == 1
+  return tonumber(row) == 1
 end
 
 -- Check user password.
 function user_verify(conn, plaintext)
-  local res     -- result handle
-  local row     -- one row of the sql result
-  local query   -- sql query
-
-  local fields = 'password'
-
-  local table_user = get_option("table_user")
-
   if not dbh then
     error("Missing database connection...")
   end
@@ -222,16 +131,17 @@ function user_verify(conn, plaintext)
   local username = dbh:escape(auth.get_username(conn))
 
   -- get the password for this user
-  query = string.format([[SELECT %s FROM %s WHERE name = '%s']],
-                        fields, table_user, username)
-  res = assert(dbh:execute(query))
+  local query = string.format([[SELECT password FROM auth_user WHERE username = '%s']], username)
+  local res = assert(dbh:execute(query))
 
-  row = res:fetch({}, 'a')
+  local row = res:fetch({}, 'a')
   if not row then
     -- No match
     res:close()
     return nil
   end
+
+  local ok = check_password(row.password, plaintext)
 
   -- There should be only one result
   if res:fetch() then
@@ -242,60 +152,15 @@ function user_verify(conn, plaintext)
 
   res:close()
 
-  return row.password == md5sum(plaintext)
+  return ok
 end
 
 -- save a user to the database
 function user_save(conn, password)
-  local table_user = get_option("table_user")
-
-  if not dbh then
-    error("Missing database connection...")
-  end
-
-  local username = dbh:escape(auth.get_username(conn))
-  local ipaddr = auth.get_ipaddr(conn)
-
-  -- insert the user
-  --local now = os.time()
-  local query = string.format([[INSERT INTO %s VALUES (NULL, '%s', '%s',
-                                NULL, %s, %s, '%s', '%s', 0)]],
-                              table_user, username, md5sum(password),
-                              sql_time(), sql_time(),
-                              ipaddr, ipaddr)
-  assert(dbh:execute(query))
-
-  user_log(conn, true)
 end
 
 -- log the session
 function user_log(conn, success)
-  local query   -- sql query
-
-  if not dbh then
-    error("Missing database connection...")
-  end
-
-  local table_user = get_option("table_user")
-  local table_log  = get_option("table_log")
-
-  local username = dbh:escape(auth.get_username(conn))
-  local ipaddr = auth.get_ipaddr(conn)
-  local success_str = success and 'S' or 'F'
-
-  -- update user data
-  --local now = os.time()
-  query = string.format([[UPDATE %s SET accesstime = %s, address = '%s',
-                          logincount = logincount + 1
-                          WHERE name = '%s']], table_user, sql_time(),
-                          ipaddr, username)
-  assert(dbh:execute(query))
-
-  -- insert the log row for this user
-  query = string.format([[INSERT INTO %s (name, logintime, address, succeed)
-                          VALUES ('%s', %s, '%s', '%s')]],
-                        table_log, username, sql_time(), ipaddr, success_str)
-  assert(dbh:execute(query))
 end
 
 -- **************************************************************************
@@ -304,23 +169,9 @@ end
 
 -- test and initialise the database connection
 function database_init()
-  options_init()
+  local sql = ls_postgres.postgres()
 
-  local backend = get_option("backend")
-
-  if backend == 'mysql' then
-    log.verbose('Opening MySQL database connection...')
-    return mysql_connect()
-  end
-
-  if backend == 'sqlite' then
-    log.verbose('Opening SQLite database connection...')
-    return sqlite_connect()
-  end
-
-  error(string.format(
-    'Database backend \'%s\' not supported by database.lua',
-    backend))
+  dbh = assert(sql:connect("longturn", "longturn"))
 end
 
 -- free the database connection