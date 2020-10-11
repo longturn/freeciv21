0a8c05d91c976a2b5eda6914550bd59c69ab102d
Louis Moureaux <m_louis30@yahoo.com>
2020-10-11 18:49:25 +0200

    C++ify server/srv_main.cpp

--- server/srv_main.cpp
+++ server/srv_main.cpp
@@ -265,8 +265,8 @@ void srv_init(void)
   srvarg.ranklog_filename = NULL;
   srvarg.load_filename[0] = '\0';
   srvarg.script_filename = NULL;
-  srvarg.saves_pathname = "";
-  srvarg.scenarios_pathname = "";
+  srvarg.saves_pathname = strdup("");
+  srvarg.scenarios_pathname = strdup("");
   srvarg.ruleset = NULL;
 
   srvarg.quitidle = 0;
@@ -1081,7 +1081,7 @@ static void begin_turn(bool is_new_turn)
   if (is_new_turn) {
     if (game.info.phase_mode != game.server.phase_mode_stored) {
       event_cache_phases_invalidate();
-      game.info.phase_mode = game.server.phase_mode_stored;
+      game.info.phase_mode = phase_mode_type(game.server.phase_mode_stored);
     }
   }
 
@@ -1989,7 +1989,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
 
   if (!pconn->established) {
     log_error("Received game packet %s(%d) from unaccepted connection %s.",
-              packet_name(type), type, conn_description(pconn));
+              packet_name(packet_type(type)), type, conn_description(pconn));
     return TRUE;
   }
 
@@ -2013,7 +2013,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
       return TRUE;
     }
 
-    if (!server_handle_packet(type, packet, NULL, pconn)) {
+    if (!server_handle_packet(packet_type(type), packet, NULL, pconn)) {
       log_error("Received unknown packet %d from %s.",
                 type, conn_description(pconn));
     }
@@ -2029,7 +2029,7 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
     }
     /* don't support these yet */
     log_error("Received packet %s(%d) from non-player connection %s.",
-              packet_name(type), type, conn_description(pconn));
+              packet_name(packet_type(type)), type, conn_description(pconn));
     return TRUE;
   }
 
@@ -2041,10 +2041,10 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
       /* This can happen by accident, so we don't want to print
        * out lots of error messages. Ie, we use log_debug(). */
       log_debug("Got a packet of type %s(%d) in %s.",
-                packet_name(type), type, server_states_name(S_S_OVER));
+                packet_name(packet_type(type)), type, server_states_name(S_S_OVER));
     } else {
       log_error("Got a packet of type %s(%d) outside %s.",
-                packet_name(type), type, server_states_name(S_S_RUNNING));
+                packet_name(packet_type(type)), type, server_states_name(S_S_RUNNING));
     }
     return TRUE;
   }
@@ -2053,14 +2053,14 @@ bool server_packet_input(struct connection *pconn, void *packet, int type)
 
   if (!pplayer->is_alive && type != PACKET_REPORT_REQ) {
     log_error("Got a packet of type %s(%d) from a dead player.",
-              packet_name(type), type);
+              packet_name(packet_type(type)), type);
     return TRUE;
   }
   
   /* Make sure to set this back to NULL before leaving this function: */
   pplayer->current_conn = pconn;
 
-  if (!server_handle_packet(type, packet, pplayer, pconn)) {
+  if (!server_handle_packet(packet_type(type), packet, pplayer, pconn)) {
     log_error("Received unknown packet %d from %s.",
               type, conn_description(pconn));
   }
@@ -2333,7 +2333,7 @@ void handle_player_ready(struct player *requestor,
 **************************************************************************/
 const char *aifill(int amount)
 {
-  char *limitreason = NULL;
+  const char *limitreason = NULL;
   int limit;
  
   if (game_was_started()) {
@@ -2400,9 +2400,9 @@ const char *aifill(int amount)
     sz_strlcpy(pplayer->username, _(ANON_USER_NAME));
     pplayer->unassigned_user = TRUE;
 
-    pplayer->ai_common.skill_level = game.info.skill_level;
+    pplayer->ai_common.skill_level = ai_level(game.info.skill_level);
     set_as_ai(pplayer);
-    set_ai_level_directer(pplayer, game.info.skill_level);
+    set_ai_level_directer(pplayer, ai_level(game.info.skill_level));
 
     CALL_PLR_AI_FUNC(gained_control, pplayer, pplayer);
 
