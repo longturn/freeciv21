11e26254237f6b1cbba8fe70fada3c317897e76c
᳇ Ϻ⚸Яℇ⚔ ᳇ <mlewczuk80@gmail.com>
2020-12-06 12:54:32 +0100

    Fix crash in research dialog with new colors

--- client/gui-qt/colors.cpp
+++ client/gui-qt/colors.cpp
@@ -29,37 +29,38 @@ research_color *research_color::i()
 
 void research_color::init_colors()
 {
-  *colors[0] = QColor(palette().color(QPalette::Highlight));
+  colors[0] = QColor(palette().color(QPalette::Highlight));
   // 4 darker
-  *colors[1] = QColor(palette().color(QPalette::Highlight)).lighter(20);
-  *colors[2] = QColor(palette().color(QPalette::Highlight)).lighter(40);
-  *colors[3] = QColor(palette().color(QPalette::Highlight)).lighter(60);
-  *colors[4] = QColor(palette().color(QPalette::Highlight)).lighter(80);
+  colors[1] = QColor(palette().color(QPalette::Highlight)).lighter(20);
+  colors[2] = QColor(palette().color(QPalette::Highlight)).lighter(40);
+  colors[3] = QColor(palette().color(QPalette::Highlight)).lighter(60);
+  colors[4] = QColor(palette().color(QPalette::Highlight)).lighter(80);
   // 4 lighter
-  *colors[5] = QColor(palette().color(QPalette::Highlight)).lighter(120);
-  *colors[6] = QColor(palette().color(QPalette::Highlight)).lighter(140);
-  *colors[7] = QColor(palette().color(QPalette::Highlight)).lighter(120);
+  colors[5] = QColor(palette().color(QPalette::Highlight)).lighter(120);
+  colors[6] = QColor(palette().color(QPalette::Highlight)).lighter(140);
+  colors[7] = QColor(palette().color(QPalette::Highlight)).lighter(120);
   // tech goal - green boost
-  colors[7]->setRgb(colors[0]->red() * 0.9,
-                    qMax(static_cast<int>(colors[0]->green() * 1.1), 254),
-                    colors[0]->blue() * 0.9);
-  *colors[8] = QColor(palette().color(QPalette::Highlight)).lighter(180);
-  *colors[9] = QColor(*colors[0]);
+  colors[7].setRgb(colors[0].red() * 0.9,
+                    qMax(static_cast<int>(colors[0].green() * 1.1), 254),
+                    colors[0].blue() * 0.9);
+  colors[8] = QColor(palette().color(QPalette::Highlight)).lighter(180);
+  colors[9] = QColor(colors[0]);
   // red boost - current tech
-  colors[9]->setRgb(qMax(static_cast<int>(colors[9]->red() * 1.1), 254),
-                    colors[9]->green() * 0.9, colors[9]->blue() * 0.9);
-  *colors[10] = QColor(palette().color(QPalette::AlternateBase));
-  *colors[20] = QColor(palette().color(QPalette::Text));
-  *colors[21] = QColor(palette().color(QPalette::Text)).lighter(20);
-  *colors[22] = QColor(palette().color(QPalette::Text)).lighter(40);
-  *colors[23] = QColor(palette().color(QPalette::Text)).lighter(60);
-  *colors[24] = QColor(palette().color(QPalette::Text)).lighter(80);
+  colors[9].setRgb(qMax(static_cast<int>(colors[9].red() * 1.1), 254),
+                    colors[9].green() * 0.9, colors[9].blue() * 0.9);
+  colors[10] = QColor(palette().color(QPalette::AlternateBase));
+  colors[20] = QColor(palette().color(QPalette::Text));
+  colors[21] = QColor(palette().color(QPalette::Text)).lighter(20);
+  colors[22] = QColor(palette().color(QPalette::Text)).lighter(40);
+  colors[23] = QColor(palette().color(QPalette::Text)).lighter(60);
+  colors[24] = QColor(palette().color(QPalette::Text)).lighter(80);
   // 4 lighter
-  *colors[25] = QColor(palette().color(QPalette::Text)).lighter(120);
-  *colors[26] = QColor(palette().color(QPalette::Text)).lighter(140);
-  *colors[27] = QColor(palette().color(QPalette::Text)).lighter(160);
-  *colors[28] = QColor(palette().color(QPalette::Text)).lighter(180);
-  *colors[30] = QColor(0, 0, 0);
+  colors[25] = QColor(palette().color(QPalette::Text)).lighter(120);
+  colors[26] = QColor(palette().color(QPalette::Text)).lighter(140);
+  colors[27] = QColor(palette().color(QPalette::Text)).lighter(160);
+  colors[28] = QColor(palette().color(QPalette::Text)).lighter(180);
+  colors[30] = QColor(0, 0, 0);
+  colors_init = true;
 }
 // get color from fake object
 QColor *research_color::get_color(int c)
@@ -67,7 +68,7 @@ QColor *research_color::get_color(int c)
   if (!colors_init) {
     init_colors();
   }
-  return colors[c];
+  return &colors[c];
 }
 
 // returns color from qss fake object
