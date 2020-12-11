9202b9ae6c80f45264ce5382842ec8ea6bf3ac13
᳇ Ϻ⚸Яℇ⚔ ᳇ <mlewczuk80@gmail.com>
2020-12-11 22:40:00 +0100

    Added notification about awaken unit

--- server/unittools.cpp
+++ server/unittools.cpp
@@ -3391,6 +3391,11 @@ static void wakeup_neighbor_sentries(struct unit *punit)
           && can_unit_exist_at_tile(&(wld.map), penemy, unit_tile(penemy))) {
         set_unit_activity(penemy, ACTIVITY_IDLE);
         send_unit_info(NULL, penemy);
+        notify_player(unit_owner(penemy), unit_tile(punit), E_UNIT_ORDERS,
+                      ftc_server, _("Sentried %s saw %s %s moving at %s"),
+                      unit_tile_link(penemy),
+                      nation_rule_name(nation_of_unit(punit)),
+                      unit_link(punit), tile_link(punit->tile));
       }
     }
     unit_list_iterate_end;
