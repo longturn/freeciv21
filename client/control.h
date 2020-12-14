83f66454cfa470f5d104796491e4fed3e2bc4ca4
᳇ Ϻ⚸Яℇ⚔ ᳇ <mlewczuk80@gmail.com>
2020-12-15 00:55:27 +0100

    Enums in control.h

--- client/control.h
+++ client/control.h
@@ -12,7 +12,6 @@
       \____/        ********************************************************/
 #pragma once
 
-
 /* common */
 #include "actions.h"
 #include "packets.h"
@@ -112,22 +111,22 @@ void request_unit_convert(struct unit *punit);
 void request_units_wait(struct unit_list *punits);
 void request_unit_wakeup(struct unit *punit);
 
-#define SPECENUM_NAME unit_select_type_mode
-#define SPECENUM_VALUE0 SELTYPE_SINGLE
-#define SPECENUM_VALUE1 SELTYPE_SAME
-#define SPECENUM_VALUE2 SELTYPE_ALL
-#include "specenum_gen.h"
+enum unit_select_location_mode {
+  SELLOC_UNITS, /* Units on tile. */
+  SELLOC_TILE,  /* Tile. */
+  SELLOC_CONT,  /* Continent. */
+  SELLOC_LAND,  /* Move type: land. */
+  SELLOC_SEA,   /* Move type: sea. */
+  SELLOC_BOTH,  /* Move type: both. */
+  SELLOC_WORLD, /* World. */
+  SELLOC_COUNT
+};
 
-#define SPECENUM_NAME unit_select_location_mode
-#define SPECENUM_VALUE0 SELLOC_UNITS /* Units on tile. */
-#define SPECENUM_VALUE1 SELLOC_TILE  /* Tile. */
-#define SPECENUM_VALUE2 SELLOC_CONT  /* Continent. */
-#define SPECENUM_VALUE3 SELLOC_LAND  /* Move type: land. */
-#define SPECENUM_VALUE4 SELLOC_SEA   /* Move type: sea. */
-#define SPECENUM_VALUE5 SELLOC_BOTH  /* Move type: both. */
-#define SPECENUM_VALUE6 SELLOC_WORLD /* World. */
-#define SPECENUM_COUNT SELLOC_COUNT
-#include "specenum_gen.h"
+enum unit_select_type_mode {
+  SELTYPE_SINGLE,
+  SELTYPE_SAME,
+  SELTYPE_ALL
+};
 
 void request_unit_select(struct unit_list *punits,
                          enum unit_select_type_mode seltype,
@@ -270,5 +269,3 @@ void cancel_city(struct tile *ptile);
 #define MAX_NUM_UNITS_BELOW 4
 
 extern int num_units_below;
-
-
