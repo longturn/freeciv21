c99381195a6146e482d3a4c7cb4d0e73454bd6d1
Louis Moureaux <m_louis30@yahoo.com>
2020-12-16 01:35:24 +0100

    Fix it

--- utility/megaenums.cpp
+++ utility/megaenums.cpp
@@ -5,58 +5,16 @@
 #include <string>
 #include <vector>
 
-#define STRING_REMOVE_CHAR(str, ch) str.erase(std::remove(str.begin(), str.end(), ch), str.end())
-
-std::vector<std::string> splitString(std::string str, char sep = ',') {
-    std::vector<std::string> vecString;
-    std::string item;
-
-    std::stringstream stringStream(str);
-
-    while (std::getline(stringStream, item, sep))
-    {
-        vecString.push_back(item);
-    }
-
-    return vecString;
-}
-
-template <typename T>
-std::map<T, std::string> generateEnumMap(std::string strMap)
+std::vector<std::string> splitString(std::string str, char sep = ',')
 {
-    STRING_REMOVE_CHAR(strMap, ' ');
-    STRING_REMOVE_CHAR(strMap, '(');
+  std::vector<std::string> vecString;
+  std::string item;
 
-    std::vector<std::string> enumTokens(splitString(strMap));
-    std::map<T, std::string> retMap;
-    T inxMap;
+  std::stringstream stringStream(str);
 
-    inxMap = 0;
-    for (auto iter = enumTokens.begin(); iter != enumTokens.end(); ++iter)
-    {
-        // Token: [EnumName | EnumName=EnumValue]
-        std::string enumName;
-        if (iter->find('=') == std::string::npos)
-        {
-            enumName = *iter;
-        }
-        else
-        {
-            std::vector<std::string> enumNameValue(splitString(*iter, '='));
-            enumName = enumNameValue[0];
-            //inxMap = static_cast<T>(enumNameValue[1]);
-            if (std::is_unsigned<T>::value)
-            {
-                inxMap = static_cast<T>(std::stoull(enumNameValue[1], 0, 0));
-            }
-            else
-            {
-                inxMap = static_cast<T>(std::stoll(enumNameValue[1], 0, 0));
-            }
-        }
-        retMap[inxMap++] = enumName;
-    }
+  while (std::getline(stringStream, item, sep)) {
+    vecString.push_back(item);
+  }
 
-    return retMap;
+  return vecString;
 }
-
--- utility/megaenums.h
+++ utility/megaenums.h
@@ -9,14 +9,49 @@
 
 std::vector<std::string> splitString(std::string str, char sep = ',');
 
-#define DECLARE_ENUM_WITH_TYPE(E, T, ...)                                                                    \
-    enum E : T                                                                                          \
-    {                                                                                                         \
-        __VA_ARGS__                                                                                           \
-    };                                                                                                        \
-inline std::map<T, std::string> E##MapName(generateEnumMap<T>(#__VA_ARGS__));                                    \
-inline std::string E##_name(E enumTmp) { return E##MapName[static_cast<T>(enumTmp)]; }                          \
+#define STRING_REMOVE_CHAR(str, ch)                                         \
+  str.erase(std::remove(str.begin(), str.end(), ch), str.end())
 
-#define DECLARE_ENUM(E, ...) DECLARE_ENUM_WITH_TYPE(E, int32_t, __VA_ARGS__)
 template <typename T>
-std::map<T, std::string> generateEnumMap(std::string strMap);
+std::map<T, std::string> generateEnumMap(std::string strMap)
+{
+  STRING_REMOVE_CHAR(strMap, ' ');
+  STRING_REMOVE_CHAR(strMap, '(');
+
+  std::vector<std::string> enumTokens(splitString(strMap));
+  std::map<T, std::string> retMap;
+  T inxMap;
+
+  inxMap = 0;
+  for (auto iter = enumTokens.begin(); iter != enumTokens.end(); ++iter) {
+    /* Token: [EnumName | EnumName=EnumValue] */
+    std::string enumName;
+    if (iter->find('=') == std::string::npos) {
+      enumName = *iter;
+    } else {
+      std::vector<std::string> enumNameValue(splitString(*iter, '='));
+      enumName = enumNameValue[0];
+      if (std::is_unsigned<T>::value) {
+        inxMap = static_cast<T>(std::stoull(enumNameValue[1], 0, 0));
+      } else {
+        inxMap = static_cast<T>(std::stoll(enumNameValue[1], 0, 0));
+      }
+    }
+    retMap[inxMap++] = enumName;
+  }
+
+  return retMap;
+}
+
+#define DECLARE_ENUM_WITH_TYPE(E, T, ...)                                   \
+  enum E : T { __VA_ARGS__ };                                               \
+                                                                            \
+  inline const auto &E##MapName()                                           \
+  {                                                                         \
+    static auto map = generateEnumMap<T>(#__VA_ARGS__);                     \
+    return map;                                                             \
+  }                                                                         \
+  inline std::string E##_name(E enumTmp)                                    \
+  {                                                                         \
+    return E##MapName().at(static_cast<T>(enumTmp));                        \
+  }
