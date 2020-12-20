939a59a9136dd7eb8a5d01b23fc3ec498f85f173
Louis Moureaux <m_louis30@yahoo.com>
2020-12-21 00:02:04 +0100

    Fix one more division by zero

--- common/road.cpp
+++ common/road.cpp
@@ -389,7 +389,7 @@ int count_river_type_near_tile(const struct tile *ptile,
   adjc_iterate_end;
 
   if (percentage) {
-    count = count * 100 / total;
+    count = count * 100 / std::max(1, total);
   }
   return count;
 }
