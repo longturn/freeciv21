ff8ee7b3380f33dc43cf1b3d0ca77ba3a4b6830a
Louis Moureaux <m_louis30@yahoo.com>
2020-12-26 00:36:27 +0100

    Improve travis-clang-format.sh
    * Don't let 'which' print anything
    * Prettier display of the clang-format version
    * Don't error out if format.diff isn't empty but doesn't look like a diff

--- scripts/travis-clang-format.sh
+++ scripts/travis-clang-format.sh
@@ -8,19 +8,20 @@ dirs="ai common client server tools utility"
 # Debian-based systems have clang-format-xx, use the most recent
 clang_format=$(which clang-format)
 for version in $(seq 7 11); do
-  which 2>/dev/null clang-format-$version && clang_format=clang-format-$version
+  which 2>&1 >/dev/null clang-format-$version && clang_format=clang-format-$version
 done
 
 [ -z "$clang_format" ] && exit 1
 
-echo "Using $clang_format:" >&2
+echo -n "Using $clang_format: " >&2
 $clang_format --version >&2
 
 # Run it on all files changed with respect to the target branch
 # Save the diff, then display it and exit 1 if it wasn't empty.
 git-clang-format --diff $(git merge-base $TRAVIS_BRANCH HEAD) HEAD >format.diff
 
-if [ ! -s format.diff ]; then
+# Error out only if it looks like a patch
+if grep format.diff 'diff --git' &>/dev/null; then
   echo 'clang-format found formatting mistakes in some of the files you changed.' >&2
   echo 'Here are the changes you need to apply:' >&2
   echo '===================================================================' >&2
