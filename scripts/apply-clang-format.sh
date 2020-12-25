01581356424d8ab1304c00d5719ed8add5fffabf
Louis Moureaux <m_louis30@yahoo.com>
2020-12-25 18:04:19 +0100

    Use the most recent clang-format available

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -4,4 +4,17 @@
 # type every time.
 
 dirs="ai common client server tools utility"
-find $dirs -\( -name '*.h' -or -name '*.cpp' -\) -exec clang-format -i '{}' \;
+
+# Debian-based systems have clang-format-xx, use the most recent
+clang_format=$(which clang-format)
+for version in $(seq 7 11); do
+  which 2>/dev/null clang-format-$version && clang_format=clang-format-$version
+done
+
+[ -z "$clang_format" ] && exit 1
+
+echo "Using $clang_format:" >&2
+$clang_format --version >&2
+
+# Run it on all files
+find $dirs -\( -name '*.h' -or -name '*.cpp' -\) -exec $clang_format -i '{}' \;
