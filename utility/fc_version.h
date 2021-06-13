2990718a7820c69d1c85ab860dc795e7ae8369d3
jwrober <jwrober@gmail.com>
2021-06-13 13:46:47 -0700

    Bump version to 3.0.20210613.1-alpha

--- utility/fc_version.h
+++ utility/fc_version.h
@@ -2,13 +2,13 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 0
-#define PATCH_VERSION 92
+#define PATCH_VERSION 20210613
 #define EMERGENCY_VERSION 1
 #ifndef VERSION_LABEL
-#define VERSION_LABEL "-haxx"
+#define VERSION_LABEL "-alpha"
 #endif
 #ifndef VERSION_STRING
-#define VERSION_STRING "3.0.1337.1-haxx"
+#define VERSION_STRING "3.0.20210613.1-alpha"
 #endif
 
 #define NETWORK_CAPSTRING "+Freeciv21.21April13"
