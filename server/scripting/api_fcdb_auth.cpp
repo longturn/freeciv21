3169eb0985b9e660173eddaccf91b9b1052dbacd
Louis Moureaux <m_louis30@yahoo.com>
2021-06-28 04:39:44 +0200

    Port the tolua_fcdb.pkg to Sol2
    This removes most of the need for intermediate functions and also
    doesn't require running an external tool.
    
    See #504.

--- server/scripting/api_fcdb_auth.cpp
+++ server/scripting/api_fcdb_auth.cpp
@@ -18,37 +18,25 @@
 // common
 #include "connection.h"
 
-/* common/scriptcore */
-#include "luascript.h"
-
 // server
 #include "auth.h"
-
-/* server/scripting */
-#include "script_fcdb.h"
 
 #include "api_fcdb_auth.h"
 
 /**
    Get the username.
  */
-const char *api_auth_get_username(lua_State *L, Connection *pconn)
+const char *api_auth_get_username(Connection *pconn)
 {
-  LUASCRIPT_CHECK_STATE(L, NULL);
-  LUASCRIPT_CHECK_SELF(L, pconn, NULL);
   fc_assert_ret_val(conn_is_valid(pconn), NULL);
-
   return auth_get_username(pconn);
 }
 
 /**
    Get the ip address.
  */
-const char *api_auth_get_ipaddr(lua_State *L, Connection *pconn)
+const char *api_auth_get_ipaddr(Connection *pconn)
 {
-  LUASCRIPT_CHECK_STATE(L, NULL);
-  LUASCRIPT_CHECK_SELF(L, pconn, NULL);
   fc_assert_ret_val(conn_is_valid(pconn), NULL);
-
   return auth_get_ipaddr(pconn);
 }
