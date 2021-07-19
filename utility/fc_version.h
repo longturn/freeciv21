ab15e52150c2869f077b2f52748b740f16f580a8
James Robertson <jwrober@gmail.com>
2021-07-19 17:05:31 -0500

    Bump Version

--- utility/fc_version.h
+++ utility/fc_version.h
@@ -2,13 +2,13 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 0
-#define PATCH_VERSION 20210613
-#define EMERGENCY_VERSION 1
+#define PATCH_VERSION 20210719
+#define EMERGENCY_VERSION 2
 #ifndef VERSION_LABEL
 #define VERSION_LABEL "-alpha"
 #endif
 #ifndef VERSION_STRING
-#define VERSION_STRING "3.0.20210613.1-alpha"
+#define VERSION_STRING "3.0.20210719.2-alpha"
 #endif
 
 #define NETWORK_CAPSTRING "+Freeciv21.21April13"
