91d01363dd3ee5a048bf710e62075207bd45879d
jwrober <jwrober@gmail.com>
2021-08-30 11:16:55 -0500

    Bump version to alpha 4

--- utility/fc_version.h
+++ utility/fc_version.h
@@ -2,16 +2,16 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 0
-#define PATCH_VERSION 20210721
-#define EMERGENCY_VERSION 3
+#define PATCH_VERSION 20210901
+#define EMERGENCY_VERSION 4
 #ifndef VERSION_LABEL
 #define VERSION_LABEL "-alpha"
 #endif
 #ifndef VERSION_STRING
-#define VERSION_STRING "3.0.20210721.3-alpha"
+#define VERSION_STRING "3.0.20210901.4-alpha"
 #endif
 
-#define NETWORK_CAPSTRING "+Freeciv21.21April13"
+#define NETWORK_CAPSTRING "+Freeciv.Devel-3.1-2020.Sep.10"
 
 #ifndef FOLLOWTAG
 #define FOLLOWTAG "S_HAXXOR"
