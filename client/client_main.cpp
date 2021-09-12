299261b889e8c4784fa82795da49634d08d2889c
Louis Moureaux <m_louis30@yahoo.com>
2021-09-13 00:39:15 +0200

    Disable menu music when autoconnecting
    People using -a probably want to get to the game faster and won't spend ages in
    the menus. Disable the menu music in that case. Keep it when disconnecting from
    a game.
    
    Closes #626.

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -606,7 +606,11 @@ int client_main(int argc, char *argv[])
   }
 
   audio_real_init(sound_set_name, music_set_name, sound_plugin_name);
-  start_menu_music(QStringLiteral("music_menu"), NULL);
+  if (!auto_connect) {
+    // People autoconnecting won't stay in the menus for long. Avoid starting
+    // music that will be stopped immediately.
+    start_menu_music(QStringLiteral("music_menu"), NULL);
+  }
 
   editor_init();
 
