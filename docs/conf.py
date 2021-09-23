27323b94d2272fe4d6ef8d7bd96a4f5d3c8ecdc9
James Robertson <jwrober@gmail.com>
2021-09-23 18:12:40 -0500

    Remove theme overrides

--- docs/conf.py
+++ docs/conf.py
@@ -97,11 +97,11 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-        ],
-    }
+#html_context = {
+#    'css_files': [
+#        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
+#        ],
+#    }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
