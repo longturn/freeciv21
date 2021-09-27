3a8064196f4d2c4ae1f57e70cb444a635b8eea7d
James Robertson <jwrober@gmail.com>
2021-09-27 15:55:26 -0500

    Bump version to Alpha8

--- utility/fc_version.h
+++ utility/fc_version.h
@@ -2,13 +2,13 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 0
-#define PATCH_VERSION 20210912
-#define EMERGENCY_VERSION 6
+#define PATCH_VERSION 20211008
+#define EMERGENCY_VERSION 8
 #ifndef VERSION_LABEL
 #define VERSION_LABEL "-alpha"
 #endif
 #ifndef VERSION_STRING
-#define VERSION_STRING "3.0.20210912.6-alpha"
+#define VERSION_STRING "3.0.202110008.8-alpha"
 #endif
 
 #define NETWORK_CAPSTRING "+Freeciv21.21April13"
