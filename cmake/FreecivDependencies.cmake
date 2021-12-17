ffc1f8f94e92b6d1429790afec0d1d909175a7a1
Zoltan Zarkov <zeko@freeciv.xyz>
2021-12-17 16:45:14 -0600

    Change MacOS port to vcpkg

--- cmake/FreecivDependencies.cmake
+++ cmake/FreecivDependencies.cmake
@@ -1,4 +1,5 @@
 include(CheckCSourceCompiles)
+include(CheckSymbolExists)
 include(CheckIncludeFile)
 include(CheckFunctionExists)
 include(CheckTypeSize)
@@ -25,6 +26,7 @@ find_package(Qt5 5.10 COMPONENTS Core Network REQUIRED)
 # Required for utility
 if(FREECIV_ENABLE_SERVER)
   find_package(Readline REQUIRED)
+  check_symbol_exists(rl_completion_suppress_append "readline/readline.h" HAVE_SUPPRESS_APPEND)
 endif()
 
 # Internationalization
@@ -91,7 +93,7 @@ find_package(Lua 5.3 REQUIRED)
 
 # Create an imported target since it's not created by CMake :(
 # Get a library name for IMPORTED_LOCATION
-if (NOT EMSCRIPTEN)
+if (NOT EMSCRIPTEN AND NOT APPLE)
   add_library(lua UNKNOWN IMPORTED GLOBAL)
   list(GET LUA_LIBRARIES 0 loc)
   set_target_properties(lua PROPERTIES
