a909454cc6d1ae0c08d88d7bc9359e9318afc4a7
zekoz <zeko@freeciv.xyz>
2022-01-07 10:07:43 -0600

    Add Royale ruleset, adapted from LTT (#804)
    Add Royale ruleset, adapted from LTT

---
+++ data/royale/parser.lua
@@ -0,0 +1,10 @@
+-- Copyright (c) 2007-2020 Freeciv21 and Freeciv contributors. This file is
+--   part of Freeciv21. Freeciv21 is free software: you can redistribute it
+--   and/or modify it under the terms of the GNU General Public License
+--   as published by the Free Software Foundation, either version 3
+--   of the License,  or (at your option) any later version.
+--   You should have received a copy of the GNU General Public License
+--   along with Freeciv21. If not, see https://www.gnu.org/licenses/.
+
+-- This file is for lua-functionality for parsing luadata.txt
+-- of this ruleset.
