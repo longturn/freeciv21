d71d344694a2d1f2c91dd65980a77e52c40a5cab
James Robertson <jwrober@gmail.com>
2022-01-21 14:38:28 -0600

    Update cmake/AutoRevision.cmake to check for valid repo

--- cmake/AutoRevision.cmake
+++ cmake/AutoRevision.cmake
@@ -7,33 +7,42 @@ find_package(Git)
 
 if(Git_FOUND)
 
-  # get the value of the git hash at HEAD to 5 chars
-  execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short=5 HEAD
-                  OUTPUT_VARIABLE FC21_REV_HEAD_HASH_H
+  # run git status to see if we are in a real local repo
+  execute_process(COMMAND ${GIT_EXECUTABLE} status
+                  OUTPUT_VARIABLE check-status
                   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  # convert the hexadecimal hash to a decimal number to support the project()
-  math(EXPR FC21_REV_HEAD_HASH_D "0x${FC21_REV_HEAD_HASH_H}" OUTPUT_FORMAT DECIMAL)
-  message("-- Git HEAD Commit Hash: (hex) ${FC21_REV_HEAD_HASH_H} and (dec) ${FC21_REV_HEAD_HASH_D}")
+  if(NOT ${check-status} MATCHES "fatal: not a git repository (or any of the parent directories): .git")
 
-  # get a temp value of the full commit hash of the latest tag that is active
-  execute_process(COMMAND ${GIT_EXECUTABLE} rev-list --tags --max-count=1
-                  OUTPUT_VARIABLE _output
-                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # get the value of the git hash at HEAD to 5 chars
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short=5 HEAD
+                    OUTPUT_VARIABLE FC21_REV_HEAD_HASH_H
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  # use the temp value to get the latest revision tag
-  execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags ${_output}
-                  OUTPUT_VARIABLE FC21_REV_TAG
-                  OUTPUT_STRIP_TRAILING_WHITESPACE)
-  message("-- Latest Git Tag: ${FC21_REV_TAG}")
+    # convert the hexadecimal hash to a decimal number to support the project()
+    math(EXPR FC21_REV_HEAD_HASH_D "0x${FC21_REV_HEAD_HASH_H}" OUTPUT_FORMAT DECIMAL)
+    message("-- Git HEAD Commit Hash: (hex) ${FC21_REV_HEAD_HASH_H} and (dec) ${FC21_REV_HEAD_HASH_D}")
 
-  # manipulate the tag so we can turn it into a list for use later
-  string(REPLACE "v" "" FC21_REV_TAG2 "${FC21_REV_TAG}")
-  string(REPLACE "." " " FC21_REV_TAG2 "${FC21_REV_TAG2}")
-  string(REPLACE "-" " " FC21_REV_TAG2 "${FC21_REV_TAG2}")
-  set(FC21_REV_TAG_LIST ${FC21_REV_TAG2})
-  separate_arguments(FC21_REV_TAG_LIST)
-  message("-- Latest Git Tag List: ${FC21_REV_TAG_LIST}")
+    # get a temp value of the full commit hash of the latest tag that is active
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-list --tags --max-count=1
+                    OUTPUT_VARIABLE _output
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    # use the temp value to get the latest revision tag
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags ${_output}
+                    OUTPUT_VARIABLE FC21_REV_TAG
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    message("-- Latest Git Tag: ${FC21_REV_TAG}")
+
+    # manipulate the tag so we can turn it into a list for use later
+    string(REPLACE "v" "" FC21_REV_TAG2 "${FC21_REV_TAG}")
+    string(REPLACE "." " " FC21_REV_TAG2 "${FC21_REV_TAG2}")
+    string(REPLACE "-" " " FC21_REV_TAG2 "${FC21_REV_TAG2}")
+    set(FC21_REV_TAG_LIST ${FC21_REV_TAG2})
+    separate_arguments(FC21_REV_TAG_LIST)
+    message("-- Latest Git Tag List: ${FC21_REV_TAG_LIST}")
+
+  endif()
 
 else()
 
