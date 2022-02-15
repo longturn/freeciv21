cff29a90a35dd1a87cf3fbde19a5910f6288e451
Zoltan Zarkov <zeko@freeciv.xyz>
2022-02-14 21:24:59 -0600

    Add theme-coloring of icons.

--- client/gui-qt/icons.cpp
+++ client/gui-qt/icons.cpp
@@ -10,7 +10,12 @@
 
 #include "icons.h"
 // Qt
+#include <QApplication>
 #include <QIcon>
+#include <QIconEngine>
+#include <QPainter>
+#include <QPalette>
+#include <QSvgRenderer>
 // utility
 #include "shared.h"
 
@@ -39,6 +44,68 @@ fcIcons *fcIcons::instance()
  */
 void fcIcons::drop() { NFCN_FREE(m_instance); }
 
+namespace {
+
+/**
+   Custom icon engine that accepts paths to SVG files and renders them,
+   substituting the string "#000" (including quotes) for the current global
+   palette's ButtonText color.
+ */
+class icon_engine : public QIconEngine {
+public:
+  explicit icon_engine(const QString &path) : path(path) {}
+
+  icon_engine *clone() const override { return new icon_engine(path); }
+
+  void paint(QPainter *painter, const QRect &rect, QIcon::Mode mode,
+             QIcon::State state) override
+  {
+    // For now, only use one color variant for icons.
+    Q_UNUSED(mode)
+    Q_UNUSED(state)
+
+    auto col = QApplication::palette().color(QPalette::ButtonText);
+    QString key = path + "-" + QString::number(rect.width()) + "-"
+                  + QString::number(rect.height()) + "-" + col.name();
+    QPixmap pix;
+    if (!QPixmapCache::find(key, &pix)) {
+      pix = QPixmap(rect.width(), rect.height());
+      pix.fill(Qt::transparent);
+      if (!path.isEmpty()) {
+        QSvgRenderer renderer;
+        QFile f(path);
+        QByteArray bytes;
+        if (f.open(QIODevice::ReadOnly)) {
+          bytes = f.readAll();
+        }
+        if (!bytes.isEmpty()) {
+          bytes.replace("\"#000\"",
+                        QStringLiteral("\"%1\"").arg(col.name()).toLatin1());
+        }
+        renderer.load(bytes);
+        QPainter p(&pix);
+        renderer.render(&p, QRect(0, 0, rect.width(), rect.height()));
+      }
+      QPixmapCache::insert(key, pix);
+    }
+    painter->drawPixmap(rect.topLeft(), pix);
+  }
+
+  QPixmap pixmap(const QSize &size, QIcon::Mode mode,
+                 QIcon::State state) override
+  {
+    QPixmap pix(size);
+    pix.fill(Qt::transparent);
+    QPainter painter(&pix);
+    paint(&painter, QRect(QPoint(0, 0), size), mode, state);
+    return pix;
+  }
+
+private:
+  QString path;
+};
+} // namespace
+
 /**
    Returns icon by given name
  */
@@ -46,7 +113,7 @@ QIcon fcIcons::getIcon(const QString &id)
 {
   QIcon icon;
 
-  // Try custom icon from theme
+  // Try custom icon from theme.
   icon.addFile(
       fileinfoname(get_data_dirs(),
                    qUtf8Printable(QStringLiteral("themes/gui-qt/%1/%2.svg")
@@ -54,6 +121,17 @@ QIcon fcIcons::getIcon(const QString &id)
   if (!icon.isNull()) {
     return icon;
   }
+
+  // Try SVG from icons dir.
+  QString path = fileinfoname(
+      get_data_dirs(),
+      qUtf8Printable(QStringLiteral("themes/gui-qt/icons/%1.svg").arg(id)));
+  if (!path.isEmpty()) {
+    // Special icon engine for theme colorization.
+    return QIcon(new icon_engine(path));
+  }
+
+  // Fall back to PNG.
   icon.addFile(
       fileinfoname(get_data_dirs(),
                    qUtf8Printable(QStringLiteral("themes/gui-qt/%1/%2.png")
@@ -62,13 +140,6 @@ QIcon fcIcons::getIcon(const QString &id)
     return icon;
   }
 
-  // Try icon from icons dir
-  icon.addFile(fileinfoname(
-      get_data_dirs(),
-      qUtf8Printable(QStringLiteral("themes/gui-qt/icons/%1.svg").arg(id))));
-  if (!icon.isNull()) {
-    return icon;
-  }
   icon.addFile(fileinfoname(
       get_data_dirs(),
       qUtf8Printable(QStringLiteral("themes/gui-qt/icons/%1.png").arg(id))));
