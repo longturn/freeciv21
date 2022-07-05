ce4724ec2b76c9a3932fe085824800a543bf292f
Louis Moureaux <m_louis30@yahoo.com>
2022-07-05 23:23:10 +0200

    Create a class for LAYER_[FOCUS_]UNITS
    Encapsulating unit drawing in a class will allow for more complex options
    because it reduces the conceptual complexity of the code.

---
+++ client/layer_units.h
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Louis Moureaux <m_louis30@yahoo.com>
+ *
+ * SPDX-License-Identifier: GPLv3-or-later
+ */
+
+#pragma once
+
+#include "layer.h"
+
+namespace freeciv {
+
+class layer_units : public layer {
+public:
+  explicit layer_units(struct tileset *ts, mapview_layer layer);
+  virtual ~layer_units() = default;
+
+  std::vector<drawn_sprite>
+  fill_sprite_array(const tile *ptile, const tile_edge *pedge,
+                    const tile_corner *pcorner, const unit *punit,
+                    const city *pcity,
+                    const unit_type *putype) const override;
+};
+
+} // namespace freeciv
