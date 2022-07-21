8e57ab1ae559c499def71a148602404942e4a842
Louis Moureaux <m_louis30@yahoo.com>
2022-07-21 18:24:52 +0200

    Use QString in tileset reading functions
    This makes crashes less likely since no pointer is used.

--- client/tilespec.h
+++ client/tilespec.h
@@ -117,11 +117,12 @@ bool tileset_has_error(const struct tileset *t);
 
 void finish_loading_sprites(struct tileset *t);
 
-bool tilespec_try_read(const char *tileset_name, bool verbose, int topo_id,
+bool tilespec_try_read(const QString &name, bool verbose, int topo_id,
                        bool global_default);
-bool tilespec_reread(const char *tileset_name, bool game_fully_initialized);
+bool tilespec_reread(const QString &tileset_name,
+                     bool game_fully_initialized);
 void tilespec_reread_callback(struct option *poption);
-void tilespec_reread_frozen_refresh(const char *tname);
+void tilespec_reread_frozen_refresh(const QString &name);
 
 void tileset_setup_specialist_type(struct tileset *t, Specialist_type_id id);
 void tileset_setup_unit_type(struct tileset *t, struct unit_type *punittype);
