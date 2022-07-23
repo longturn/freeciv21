6c088bfef079ebae05618d1b8aa8fe8ebe7721b5
Louis Moureaux <m_louis30@yahoo.com>
2022-07-23 16:04:19 +0200

    Introduce map_updates_handler
    This class will be used to implement the functionality of
    refresh_tile|unit|city_mapcanvas and the like. It uses listener<> to provide a
    bridge between the old C-style API and the newer object-oriented code.
    
    So far, the class can accumulate a list of updates and restitute them.
    Eventually it should be able to notify when a new frame is needed.

---
+++ client/map_updates_handler.cpp
@@ -0,0 +1,115 @@
+#include "map_updates_handler.h"
+#include "city.h"
+#include "options.h"
+#include "tilespec.h"
+
+FC_CPP_DECLARE_LISTENER(freeciv::map_updates_handler)
+
+namespace freeciv {
+
+/**
+ * @class map_updates_handler
+ * @brief Records regions of the map that should be updated
+ *
+ * This class records a list of all regions on the map that need an update,
+ * and the extent of the necessary update (as a combination of @ref
+ * update_type). It has nothing to do with tilesets and doesn't know about
+ * the map geometry, but some tilesets assumptions are hard-coded.
+ */
+
+/**
+ * Constructor
+ */
+map_updates_handler::map_updates_handler(QObject *parent) : QObject(parent)
+{
+  map_updates_handler::listen();
+}
+
+/**
+ * Clears all pending updates.
+ */
+void map_updates_handler::clear()
+{
+  m_full_update = false;
+  m_updates.clear();
+}
+
+/**
+ * Registers a city for update. If `full`, also update the city label.
+ * @see update_city_description
+ */
+void map_updates_handler::update(const city *city, bool full)
+{
+  if (!m_full_update) {
+    const auto tile = city_tile(city);
+    if (full && (gui_options.draw_map_grid || gui_options.draw_borders)) {
+      m_updates[tile] |= update_type::city_map;
+    } else {
+      // Assumption: city sprites are as big as unit sprites
+      m_updates[tile] |= update_type::unit;
+    }
+  }
+}
+
+/**
+ * Registers a tile for update. If `full`, also update the neighboring area.
+ */
+void map_updates_handler::update(const tile *tile, bool full)
+{
+  if (!m_full_update) {
+    if (full) {
+      m_updates[tile] |= update_type::tile_full;
+    } else {
+      m_updates[tile] |= update_type::tile_single;
+    }
+  }
+}
+
+/**
+ * Registers a unit for update.
+ */
+void map_updates_handler::update(const unit *unit, bool full)
+{
+  if (!m_full_update) {
+    const auto tile = unit_tile(unit);
+    if (full && gui_options.draw_native) {
+      update_all();
+    } else if (full && unit_drawn_with_city_outline(unit, true)) {
+      m_updates[tile] |= update_type::city_map;
+    } else {
+      m_updates[tile] |= update_type::unit;
+    }
+  }
+}
+
+/**
+ * Requests an update of the whole (visible) map.
+ */
+void map_updates_handler::update_all()
+{
+  m_updates.clear();
+  m_full_update = true;
+}
+
+/**
+ * Registers a city label for update.
+ * @see update(const city *, bool)
+ */
+void map_updates_handler::update_city_description(const city *city)
+{
+  if (!m_full_update) {
+    m_updates[city_tile(city)] |= update_type::city_description;
+  }
+}
+
+/**
+ * Registers a tile label for update.
+ */
+void map_updates_handler::update_tile_label(const tile *tile)
+{
+  if (!m_full_update) {
+    m_updates[tile] |= update_type::tile_label;
+  }
+}
+
+} // namespace freeciv
---
+++ client/map_updates_handler.h
@@ -0,0 +1,64 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Louis Moureaux <m_louis30@yahoo.com>
+ *
+ * SPDX-License-Identifier: GPLv3-or-later
+ */
+
+#pragma once
+
+#include <map>
+
+#include <QObject>
+
+#include "listener.h"
+
+struct city;
+struct tile;
+struct unit;
+
+namespace freeciv {
+
+class map_updates_handler : public QObject,
+                            public listener<map_updates_handler> {
+  Q_OBJECT
+
+public:
+  /**
+   * What kind of update should be performed
+   */
+  enum class update_type {
+    tile_single,
+    tile_full,
+    unit,
+    city_description,
+    city_map,
+    tile_label,
+  };
+  Q_DECLARE_FLAGS(updates, update_type)
+
+  explicit map_updates_handler(QObject *parent = nullptr);
+  virtual ~map_updates_handler() = default;
+
+  /// Allows iteration on the list of pending updates.
+  auto begin() const { return m_updates.begin(); }
+
+  /// Allows iteration on the list of pending updates.
+  auto end() const { return m_updates.begin(); }
+
+  void clear();
+
+  void update(const city *city, bool full);
+  void update(const tile *tile, bool full);
+  void update(const unit *unit, bool full);
+  void update_all();
+  void update_city_description(const city *city);
+  void update_tile_label(const tile *tile);
+
+private:
+  bool m_full_update = false;
+  std::map<const tile *, updates> m_updates;
+};
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(map_updates_handler::updates)
+
+} // namespace freeciv
