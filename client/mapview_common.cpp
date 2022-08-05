longturn/freeciv21

commit 9bd5b4a91b87fe4e859bd8439dc2408382380078

Fixed forcing full map update on city map

client/mapview_common.cpp
@@ -1319,7 +1319,7 @@ void put_drawn_sprites(struct canvas *pcanvas, float zoom, int canvas_x,
     }
   }
 }
-#include <QDebug>
+
 /************************************************************************/ /**
    Draw one layer of a tile, edge, corner, unit, and/or city onto the
    canvas at the given position.
@@ -1341,10 +1341,9 @@ void put_one_element(struct canvas *pcanvas, float zoom,
   if (ptile) {
     struct city *xcity = is_any_city_dialog_open();
     if (xcity) {
+      // Attack of dummies !
       int dummy_x, dummy_y;
-      //struct city *xcity = find_city_near_tile(ptile);
-      if (xcity
-          && !city_base_to_city_map(&dummy_x, &dummy_y, xcity, ptile)) {
+      if (!city_base_to_city_map(&dummy_x, &dummy_y, xcity, ptile)) {
         city_mode = true;
       }
     }
@@ -3620,11 +3619,9 @@ bool map_canvas_resized(int width, int height)
   mapview.store_width = full_width;
   mapview.store_height = full_height;
 
-  /* Check for what's changed. */
-  tile_size_changed =
-      (tile_width != old_tile_width || tile_height != old_tile_height);
-  size_changed = (width != old_width || height != old_height);
-
+  /* use that function to clear cache */
+  tile_size_changed = true;
+  size_changed = true;
   /* If the tile size has changed, resize the canvas. */
   if (tile_size_changed) {
     if (mapview.store) {

client/gui-qt/citydlg.cpp
@@ -2893,8 +2893,7 @@ void qtg_real_city_dialog_popup(struct city *pcity)
   city_dialog::instance()->show();
   city_dialog::instance()->activateWindow();
   city_dialog::instance()->raise();
-  dirty_all();
-  flush_dirty();
+  map_canvas_resized(mapview.width, mapview.height);
 }
 
 /************************************************************************/ /**
@@ -2913,12 +2912,17 @@ void destroy_city_dialog()
 void qtg_popdown_city_dialog(struct city *pcity)
 {
   city_dialog::instance()->hide();
+  map_canvas_resized(mapview.width, mapview.height);
 }
 
 /************************************************************************/ /**
    Close the dialogs for all cities.
  ****************************************************************************/
-void qtg_popdown_all_city_dialogs() { destroy_city_dialog(); }
+void qtg_popdown_all_city_dialogs()
+{
+  destroy_city_dialog();
+  map_canvas_resized(mapview.width, mapview.height);
+}
 
 /************************************************************************/ /**
    Refresh (update) all data for the given city's dialog.

commit ccb37dfd66b93c49056f8cb185e1c023c4898640

Resize the canvas a bit less often in map_canvas_resized
The cached mapview texture always gets resized in tile_width/height increments.
One needs to redraw only when the cache got resized, not every time. The code
doing that had been broken since 9bd5b4a91b87fe4e859bd8439dc2408382380078; fix
it.

client/mapview_common.cpp
@@ -2496,9 +2496,8 @@ void map_canvas_resized(int width, int height)
                     / (tileset_tile_height(tileset));
   int full_width = tile_width * tileset_tile_width(tileset);
   int full_height = tile_height * tileset_tile_height(tileset);
-  bool tile_size_changed;
 
-  // Resized
+  bool cache_resized = width != mapview.width || height != mapview.height;
 
   /* Since a resize is only triggered when the tile_*** changes, the canvas
    * width and height must include the entire backing store - otherwise
@@ -2508,10 +2507,8 @@ void map_canvas_resized(int width, int height)
   mapview.store_width = full_width;
   mapview.store_height = full_height;
 
-  // use that function to clear cache
-  tile_size_changed = true;
-  // If the tile size has changed, resize the canvas.
-  if (tile_size_changed) {
+  // If the cache size has changed, resize the canvas.
+  if (cache_resized) {
     if (mapview.store) {
       delete mapview.store;
       delete mapview.tmp_store;
@@ -2523,7 +2520,7 @@ void map_canvas_resized(int width, int height)
   }
 
   if (!map_is_empty() && can_client_change_view()) {
-    if (tile_size_changed) {
+    if (cache_resized) {
       if (center_tile != nullptr) {
         int x_left, y_top;
         float gui_x, gui_y;