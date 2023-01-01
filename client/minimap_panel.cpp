cfc80e559eef038622690b164412605949c79707
Louis Moureaux <m_louis30@yahoo.com>
2023-01-01 04:58:49 +0100

    Add a menu with minimap options next to the minimap
    Make the minimap options more visible and easier to change by adding them in a
    dropdown menu next to the minimap.

--- client/minimap_panel.cpp
+++ client/minimap_panel.cpp
@@ -8,9 +8,14 @@
 
 #include "minimap_panel.h"
 
+// Qt
+#include <QMenu>
+
 // client
 #include "icons.h"
 #include "mapview.h"
+#include "options.h"
+#include "overview_common.h"
 #include "page_game.h"
 #include "top_bar.h"
 
@@ -38,6 +43,10 @@ minimap_panel::minimap_panel(map_view *map, QWidget *parent)
       fcIcons::instance()->getIcon(QStringLiteral("zoom-out")));
   connect(ui.zoom_out, &QAbstractButton::clicked, map, &map_view::zoom_out);
 
+  ui.settings->setIcon(
+      fcIcons::instance()->getIcon(QStringLiteral("settings-minimap")));
+  setup_minimap_menu();
+
   connect(ui.turn_done, &QAbstractButton::clicked, top_bar_finish_turn);
 }
 
@@ -60,6 +69,75 @@ void minimap_panel::set_minimap_visible(bool visible)
   // proper QLayout...
   QApplication::postEvent(queen()->game_tab_widget,
                           new QEvent(QEvent::LayoutRequest));
+}
+
+/**
+ * Creates the menu with the minimap settings
+ */
+void minimap_panel::setup_minimap_menu()
+{
+  auto menu = new QMenu;
+
+  m_show_relief = menu->addAction(_("Show Relief"));
+  m_show_relief->setCheckable(true);
+  ;
+  m_show_relief->setChecked(gui_options->overview.layers[OLAYER_RELIEF]);
+  QObject::connect(m_show_relief, &QAction::toggled, [](bool checked) {
+    gui_options->overview.layers[OLAYER_RELIEF] = checked;
+    refresh_overview_canvas();
+  });
+
+  m_show_borders = menu->addAction(_("Show Borders"));
+  m_show_borders->setCheckable(true);
+  ;
+  m_show_borders->setChecked(gui_options->overview.layers[OLAYER_BORDERS]);
+  QObject::connect(m_show_borders, &QAction::toggled, [=](bool checked) {
+    gui_options->overview.layers[OLAYER_BORDERS] = checked;
+    m_show_borders_ocean->setEnabled(checked);
+    refresh_overview_canvas();
+  });
+
+  m_show_borders_ocean = menu->addAction(_("Show Borders on Oceans"));
+  m_show_borders_ocean->setCheckable(true);
+  ;
+  m_show_borders_ocean->setChecked(
+      gui_options->overview.layers[OLAYER_BORDERS_ON_OCEAN]);
+  m_show_borders_ocean->setEnabled(
+      gui_options->overview.layers[OLAYER_BORDERS]);
+  QObject::connect(
+      m_show_borders_ocean, &QAction::toggled, [](bool checked) {
+        gui_options->overview.layers[OLAYER_BORDERS_ON_OCEAN] = checked;
+        refresh_overview_canvas();
+      });
+
+  m_show_cities = menu->addAction(_("Show Cities"));
+  m_show_cities->setCheckable(true);
+  ;
+  m_show_cities->setChecked(gui_options->overview.layers[OLAYER_CITIES]);
+  QObject::connect(m_show_cities, &QAction::toggled, [](bool checked) {
+    gui_options->overview.layers[OLAYER_CITIES] = checked;
+    refresh_overview_canvas();
+  });
+
+  m_show_cities = menu->addAction(_("Show Units"));
+  m_show_cities->setCheckable(true);
+  ;
+  m_show_cities->setChecked(gui_options->overview.layers[OLAYER_UNITS]);
+  QObject::connect(m_show_cities, &QAction::toggled, [](bool checked) {
+    gui_options->overview.layers[OLAYER_UNITS] = checked;
+    refresh_overview_canvas();
+  });
+
+  m_show_fog = menu->addAction(_("Show Fog of War"));
+  m_show_fog->setCheckable(true);
+  ;
+  m_show_fog->setChecked(gui_options->overview.fog);
+  QObject::connect(m_show_fog, &QAction::toggled, [](bool checked) {
+    gui_options->overview.fog = checked;
+    refresh_overview_canvas();
+  });
+
+  ui.settings->setMenu(menu);
 }
 
 /**
