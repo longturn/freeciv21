8dc6091138862b175c2dab713dcd179054402e69
Louis Moureaux <m_louis30@yahoo.com>
2023-02-01 02:54:44 +0100

    Remove ColorDodge from hud_action
    ColorDodge causes issues in some cases (#1106).  Icons are always white so this
    had no effect.  Remove ColorDodge to avoid a bad surprise in the future.

--- client/hudwidget.cpp
+++ client/hudwidget.cpp
@@ -824,11 +824,9 @@ void hud_action::paintEvent(QPaintEvent *event)
   QPainter p;
 
   p.begin(this);
-  p.setCompositionMode(QPainter::CompositionMode_Source);
   p.setRenderHint(QPainter::SmoothPixmapTransform);
   icon.paint(&p, rect());
   if (focus) {
-    p.setCompositionMode(QPainter::CompositionMode_DestinationOver);
     p.fillRect(rect(), QColor(palette().color(QPalette::Highlight)));
   }
   p.end();
