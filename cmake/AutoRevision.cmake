7932194e9a36ca2ecfc185cd80e82e3488d27686
Louis Moureaux <m_louis30@yahoo.com>
2023-02-16 02:59:41 +0100

    AutoRevision: Add support for dev mode
    dev mode is enabled by putting a dev tag in AutoRevision.txt. This takes
    precedence over a tag. This means that someone checking out the vA.B-alpha.1
    tag will get a -dev version number.

--- cmake/AutoRevision.cmake
+++ cmake/AutoRevision.cmake
@@ -6,15 +6,23 @@
 # of generating the build revision. It takes an update to 3 files by hand
 # to do it manually and automates it.
 
-set(ok true) # Tracks whether we use git. Allows to limit indentation.
+# In case git doesn't work, we still need the variables set.
+#   So we get them from AutoRevision.txt instead that is updated by a GitHub Action at Release
+# See .git/workflows/release.yaml
+file(READ cmake/AutoRevision.txt FC21_REV_HEAD_HASH_H LIMIT 5)
+string(REGEX REPLACE "\n$" "" FC21_REV_HEAD_HASH_H "${FC21_REV_HEAD_HASH_H}")
 
-# Auto Revision needs git to work
-find_package(Git)
-if (NOT Git_FOUND)
-  set(ok false)
+file(READ cmake/AutoRevision.txt FC21_REV_TAG OFFSET 6)
+
+if ("${FC21_REV_TAG}" MATCHES dev)
+  set(use_git_tag FALSE)  # There is no tag while in dev mode
+else()
+  set(use_git_tag TRUE)  # Tracks whether we use git. Allows to limit indentation.
 endif()
 
-if (ok)
+# Auto Revision works best with git
+find_package(Git)
+if (Git_FOUND)
   # Get the value of the git hash at HEAD to 5 chars
   execute_process(
     COMMAND ${GIT_EXECUTABLE} --git-dir=${CMAKE_SOURCE_DIR}/.git rev-parse --short=5 HEAD
@@ -23,53 +31,41 @@ if (ok)
     RESULT_VARIABLE STATUS
     ERROR_QUIET)
 
-  if (STATUS EQUAL 0)
-    # Convert the hexadecimal hash to a decimal number to support the project()
-    math(EXPR FC21_REV_HEAD_HASH_D "0x${FC21_REV_HEAD_HASH_H}" OUTPUT_FORMAT DECIMAL)
-    message(STATUS "Git HEAD Commit Hash: (hex) ${FC21_REV_HEAD_HASH_H} and (dec) ${FC21_REV_HEAD_HASH_D}")
-  else()
+  if (NOT STATUS EQUAL 0)
     # Can't use git
     message(STATUS "Could not get revision from git")
-    set(ok false)
+    set(use_git_tag FALSE)
   endif()
+else()
+  set(use_git_tag FALSE)
 endif()
 
-if (ok)
+if (use_git_tag)
   # Use the temp value to get the latest revision tag
   execute_process(
     COMMAND ${GIT_EXECUTABLE} --git-dir=${CMAKE_SOURCE_DIR}/.git describe --tags --abbrev=0
-    OUTPUT_VARIABLE FC21_REV_TAG
+    OUTPUT_VARIABLE git_tag
     OUTPUT_STRIP_TRAILING_WHITESPACE
     RESULT_VARIABLE STATUS
     ERROR_QUIET)
 
   if (STATUS EQUAL 0)
+    set(FC21_REV_TAG ${git_tag})
     message(STATUS "Latest Git Tag: ${FC21_REV_TAG}")
   else()
     # Can't use git
     message(STATUS "Could not find a git tag")
-    set(ok false)
+    set(use_git FALSE)
   endif()
 endif()
 
-if (NOT ok)
-  # In this case git didn't work, but we still need the variables set.
-  #   So we get them from AutoRevision.txt instead that is updated by a GitHub Action at Release
-  # See .git/workflows/release.yaml
-
-  message(STATUS "Could not fetch version information from git. Using AutoRevision.txt")
-  file(READ cmake/AutoRevision.txt FC21_REV_HEAD_HASH_H LIMIT 5)
-  string(REGEX REPLACE "\n$" "" FC21_REV_HEAD_HASH_H "${FC21_REV_HEAD_HASH_H}")
-
-  # Convert the hexadecimal hash to a decimal number to support the project()
-  math(EXPR FC21_REV_HEAD_HASH_D "0x${FC21_REV_HEAD_HASH_H}" OUTPUT_FORMAT DECIMAL)
-  message(STATUS "AutoRevision HEAD Commit Hash: (hex) ${FC21_REV_HEAD_HASH_H} and (dec) ${FC21_REV_HEAD_HASH_D}")
-
-  file(READ cmake/AutoRevision.txt FC21_REV_TAG OFFSET 6)
-endif()
+# Convert the hexadecimal hash to a decimal number to support the project()
+math(EXPR FC21_REV_HEAD_HASH_D "0x${FC21_REV_HEAD_HASH_H}" OUTPUT_FORMAT DECIMAL)
+message(STATUS "Git commit hash: (hex) ${FC21_REV_HEAD_HASH_H} and (dec) ${FC21_REV_HEAD_HASH_D}")
 
 # Manipulate the tag so we can turn it into a list for use later
 string(STRIP "${FC21_REV_TAG}" FC21_REV_TAG)
+message(STATUS "Version tag: ${FC21_REV_TAG}")
 
 # Expected scheme:
 #  v3.0-dev
