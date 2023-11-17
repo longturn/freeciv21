18d62858de9bfd3ac335d3e52b2f0b9d8b532a9c
Louis Moureaux <m_louis30@yahoo.com>
2023-11-17 01:07:40 +0100

    Implement mouse interaction for the multi slider
    Mouse interaction for the multi slider bears a lot to fc_double_edge. It was
    improved in a few areas:
    
     * Single clicks no longer make the handles jump around. Drag or double click
       is now required.
     * Better rounding results in smoother moving of the handles.
     * Small visual hints indicate which handle is active.
    
    The visual style is significantly different. More work is likely needed to
    properly integrate the widget with Classic (though it is usable).

--- client/widgets/multi_slider.h
+++ client/widgets/multi_slider.h
@@ -10,41 +10,34 @@
 
 namespace freeciv {
 
-/**
- * \brief A widget that lets the user distribute a fixed number of items across
- *        multiple categories.
- *
- * Assumptions:
- * - Categories are identified by their id
- * - Categories are not added or removed dynamically
- * - No category can have a negative number of items
- * - Category names are translated
- * - Icons for all categories are equally sized
- * - Maximum is exclusive
- *
- * If space allows, one icon is used to represent one item.
- * TODO tooltips
- */
-class multi_slider: public QWidget
-{
+class multi_slider : public QWidget {
   Q_OBJECT
 
-  struct category
-  {
-    QString name;
+  struct category {
     QPixmap icon;
     int minimum = 0, maximum = std::numeric_limits<int>::max();
 
-    bool allowed(int value) const { return value >= minimum && value < maximum; }
+    /// Checks if the category could take some value
+    bool allowed(int value) const
+    {
+      return value >= minimum && value <= maximum;
+    }
+  };
+
+  struct handle {
+    int index;
+    int location;
   };
 
 public:
   explicit multi_slider(QWidget *parent = nullptr);
   virtual ~multi_slider() = default;
 
-  std::size_t add_category(const QString &name, const QPixmap &icon);
+  std::size_t add_category(const QPixmap &icon);
   void set_range(std::size_t category, int min, int max);
 
+  /// Retrieves the number of items in each category.
+  std::vector<int> values() const { return m_values; }
   void set_values(const std::vector<int> &values);
 
   QSize sizeHint() const override;
@@ -52,26 +45,56 @@ public:
 
   std::size_t total() const;
 
+signals:
+  void values_changed(const std::vector<int> &values) const;
+
 protected:
   bool event(QEvent *event) override;
+
   void focusInEvent(QFocusEvent *event) override;
   void keyPressEvent(QKeyEvent *event) override;
+  void leaveEvent(QEvent *event) override;
+  void mouseDoubleClickEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *event) override;
   void paintEvent(QPaintEvent *event) override;
+  void resizeEvent(QResizeEvent *event) override;
 
 private:
   void exchange(std::size_t giver, std::size_t taker, int amount);
-  bool exchange(std::size_t taker, int amount);
+  bool grab_item(std::size_t taker, int amount, bool from_left = true,
+                 bool from_right = true);
 
   void focus_some_category();
   bool move_focus(bool forward);
 
-  std::vector<int> visible_handles() const;
+  int handle_near(const QPoint &where);
+  bool move_handle(int handle, const QPoint &where);
 
-  // Invariant: m_categories.size() == m_handles.size()
+  void update_cached_geometry();
+  std::vector<handle> visible_handles() const;
+
+  /// Category data
   std::vector<category> m_categories;
+  // Invariant: m_categories.size() == m_handles.size()
+
+  /// Number of items in each category
   std::vector<int> m_values;
-  int m_total; // Cached
+
+  /// Index of the category receiving keyboard input
   int m_focused_category = 0;
+
+  /// Index of the handle being dragged with the mouse
+  int m_closest_handle = -1;
+  int m_dragged_handle = -1;
+
+  /// Cached geometry information
+  struct {
+    int icons_width = 1;   ///< Width of the area covered with icons
+    int left_margin = 0;   ///< Empty space left of the icons
+    double item_width = 1; ///< The logical width of one item
+  } m_geom;
 };
 
 } // namespace freeciv
