longturn/freeciv21

commit f2f842177bf76321fa47a96c5c1d5a7bca10040a

new tilespec flag: 'terrain-specific-extras'

client/tileset/tilespec.cpp
@@ -87,7 +87,8 @@
 
 #define TILESPEC_CAPSTR                                                     \
   "+Freeciv-tilespec-Devel-2019-Jul-03 duplicates_ok precise-hp-bars "      \
-  "unlimited-unit-select-frames unlimited-upkeep-sprites hex_corner"
+  "unlimited-unit-select-frames unlimited-upkeep-sprites hex_corner "       \
+  "terrain-specific-extras"
 /*
  * Tilespec capabilities acceptable to this program:
  *

commit 9d8b9a1f0de5288ecf19071201da404e78d0926f

fix assertion pterrain failed bug
Introduced by #2252. Fix is more of a patch than an actual solution.
Co-authored-by: Louis Moureaux <m_louis30@yahoo.com>

client/tileset/tilespec.cpp
@@ -4792,31 +4792,44 @@ fill_sprite_array(struct tileset *t, enum mapview_layer layer,
     break;
 
   case LAYER_ROADS:
-    extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_ALL_SEPARATE], pextra)
-    {
-      if (is_extra_drawing_enabled(pextra)) {
-        fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
-                                   tterrain_near, pterrain, pcity);
+    /**
+     * Future code-dweller beware: We do not fully understand how
+     * fill_crossing_sprite_array works for the ESTYLE_ROAD_PARITY_COMBINED
+     * sprites. We do know that our recent changes for the terrain-specific
+     * extras caused an issue when the client tried to assemble an edge tile
+     * where one edge is null. We have found that wrapping all of this under
+     * `if (ptile)` solved the error, and we will leave it there. Good luck.
+     * HF and LM.
+     */
+    if (ptile) {
+      extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_ALL_SEPARATE],
+                              pextra)
+      {
+        if (is_extra_drawing_enabled(pextra)) {
+          fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
+                                     tterrain_near, pterrain, pcity);
+        }
       }
-    }
-    extra_type_list_iterate_end;
-    extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_PARITY_COMBINED],
-                            pextra)
-    {
-      if (is_extra_drawing_enabled(pextra)) {
-        fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
-                                   tterrain_near, pterrain, pcity);
+      extra_type_list_iterate_end;
+      extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_PARITY_COMBINED],
+                              pextra)
+      {
+        if (is_extra_drawing_enabled(pextra)) {
+          fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
+                                     tterrain_near, pterrain, pcity);
+        }
       }
-    }
-    extra_type_list_iterate_end;
-    extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_ALL_COMBINED], pextra)
-    {
-      if (is_extra_drawing_enabled(pextra)) {
-        fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
-                                   tterrain_near, pterrain, pcity);
+      extra_type_list_iterate_end;
+      extra_type_list_iterate(t->style_lists[ESTYLE_ROAD_ALL_COMBINED],
+                              pextra)
+      {
+        if (is_extra_drawing_enabled(pextra)) {
+          fill_crossing_sprite_array(t, pextra, sprs, textras, textras_near,
+                                     tterrain_near, pterrain, pcity);
+        }
       }
+      extra_type_list_iterate_end;
     }
-    extra_type_list_iterate_end;
     break;
 
   case LAYER_SPECIAL1: