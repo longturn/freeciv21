9254ee0347c578ae84744430f2d3d8f22928bb12
Louis Moureaux <m_louis30@yahoo.com>
2024-06-13 02:45:27 +0200

    Flush attributes as soon as they are set
    This is inefficient in terms of network traffic, but it means governors
    are saved immediately, which is usually what the user wants.

--- client/attribute.cpp
+++ client/attribute.cpp
@@ -352,6 +352,9 @@ void attribute_set(int key, int id, int x, int y, size_t data_length,
   } else {
     attribute_hash->remove(akey);
   }
+
+  // Sync with the server
+  attribute_flush();
 }
 
 /**
