cb87505062db91928bd917ebaf1e7cb7e17f46cf
Louis Moureaux <m_louis30@yahoo.com>
2024-08-06 02:45:48 +0200

    Factor out the islands map generators
    This breaks up the very large mapgen.cpp further, almost bringing it
    down to a manageable size.

---
+++ server/generator/islands.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPLv3-or-later
+// SPDX-FileCopyrightText: Freeciv21 authors
+// SPDX-FileCopyrightText: Freeciv authors
+// SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
+
+void mapgenerator2();
+void mapgenerator3();
+void mapgenerator4();
+
+void island_terrain_init();
+void island_terrain_free();
