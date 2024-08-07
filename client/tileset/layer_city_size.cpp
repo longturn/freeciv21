c8a6cf0a0362386e87b754060725242351342da8
Louis Moureaux <m_louis30@yahoo.com>
2024-08-07 15:43:55 +0200

    Migrate LAYER_CITY2 to class layer_city_size

---
+++ client/tileset/layer_city_size.cpp
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPLv3-or-later
+// SPDX-FileCopyrightText: Freeciv authors
+// SPDX-FileCopyrightText: Freeciv21 authors
+// SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
+
+#include "layer_city_size.h"
+
+#include "citybar.h"
+#include "tilespec.h"
+
+namespace freeciv {
+
+layer_city_size::layer_city_size(struct tileset *ts)
+    : freeciv::layer(ts, LAYER_CITY2)
+{
+}
+
+void layer_city_size::load_sprites()
+{
+  // Digit sprites. We have a cascade of fallbacks.
+  QStringList patterns = {QStringLiteral("city.size_%1")};
+  assign_digit_sprites(tileset(), m_units.data(), m_tens.data(),
+                       m_hundreds.data(), patterns);
+}
+
+/**
+ * Fill in the given sprite array with any needed city size sprites.
+ */
+std::vector<drawn_sprite>
+layer_city_size::fill_sprite_array(const tile *ptile, const tile_edge *pedge,
+                                   const tile_corner *pcorner,
+                                   const unit *punit) const
+{
+  if (!gui_options->draw_cities || citybar_painter::current()->has_size()) {
+    return {};
+  }
+
+  auto pcity = tile_city(ptile);
+  if (!pcity) {
+    return {};
+  }
+
+  // Now draw
+  std::vector<drawn_sprite> sprs;
+  auto size = city_size_get(pcity);
+
+  // Units
+  auto sprite = m_units[size % NUM_TILES_DIGITS];
+  sprs.emplace_back(tileset(), sprite);
+
+  // Tens
+  size /= NUM_TILES_DIGITS;
+  if (size > 0 && (sprite = m_tens[size % NUM_TILES_DIGITS])) {
+    sprs.emplace_back(tileset(), sprite);
+  }
+
+  // Hundreds (optional)
+  size /= NUM_TILES_DIGITS;
+  if (size > 0 && (sprite = m_hundreds[size % NUM_TILES_DIGITS])) {
+    sprs.emplace_back(tileset(), sprite);
+    // Divide for the warning: warn for thousands if we had a hundreds sprite
+    size /= NUM_TILES_DIGITS;
+  }
+
+  // Warn if the city is too large (only once by tileset).
+  if (size > 0 && !m_warned) {
+    qInfo(_("Tileset \"%s\" doesn't support big city sizes, such as %d. "
+            "Size not displayed as expected."),
+          tileset_name_get(tileset()), size);
+    m_warned = true;
+  }
+
+  return sprs;
+}
+
+} // namespace freeciv
---
+++ client/tileset/layer_city_size.h
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPLv3-or-later
+// SPDX-FileCopyrightText: Freeciv authors
+// SPDX-FileCopyrightText: Freeciv21 authors
+// SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
+
+#pragma once
+
+#include "layer.h"
+
+class QPixmap;
+
+namespace freeciv {
+
+class layer_city_size : public layer {
+public:
+  explicit layer_city_size(struct tileset *ts);
+  virtual ~layer_city_size() = default;
+
+  void load_sprites() override;
+
+  std::vector<drawn_sprite>
+  fill_sprite_array(const tile *ptile, const tile_edge *pedge,
+                    const tile_corner *pcorner,
+                    const unit *punit) const override;
+
+private:
+  mutable bool m_warned = false; ///< Did we warn the user?
+
+  std::array<QPixmap *, NUM_TILES_DIGITS> m_units, m_tens, m_hundreds;
+};
+
+} // namespace freeciv
