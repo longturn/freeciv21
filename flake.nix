e392da56310cd61b6d9ecf9aabf450183808be8a
XHawk87 <hawk87@hotmail.co.uk>
2025-04-13 23:25:57 +0100

    #2633 Added debug derivation to flake.nix

--- flake.nix
+++ flake.nix
@@ -12,9 +12,9 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        freeciv21 =
-          pkgs.stdenv.mkDerivation {
-            name = "freeciv21";
+        
+        # Common derivation attributes
+        commonAttrs = {
             src = self;
             nativeBuildInputs = [ pkgs.kdePackages.wrapQtAppsHook ];
             buildInputs = with pkgs;[
@@ -33,23 +33,49 @@
               SDL2_mixer.dev
               # KArchive dep is automatically satisfied in x86_64-darwin
             ] ++ (if system == "x86_64-darwin" then [ ] else [ kdePackages.karchive ]);
+
+            # Installing simply means copying all files to the output directory
+            installPhase = ''# Build source files and copy them over.
+                cmake --build build --target install
+                cp -R ${pkgs.libertinus}/share/fonts $out/share
+                ln -s $out/bin/freeciv21-client $out/bin/freeciv21
+            '';
+        };
+
+        # Release build
+        freeciv21 = pkgs.stdenv.mkDerivation (commonAttrs // {
+            name = "freeciv21";
             buildPhase = ''
               mkdir -p $out/
               cmake . -B build -G Ninja \
                 -DCMAKE_INSTALL_PREFIX=$out \
+                -DCMAKE_BUILD_TYPE=Release \
                 -DFREECIV_DOWNLOAD_FONTS=0
             '';
-            # Installing simply means copying all files to the output directory
-            installPhase = ''# Build source files and copy them over.
-            cmake --build build --target install
-            cp -R ${pkgs.libertinus}/share/fonts $out/share
-            ln -s $out/bin/freeciv21-client $out/bin/freeciv21
-        '';
-          };
+
+        });
+
+        # Debug build
+        freeciv21-debug = pkgs.stdenv.mkDerivation (commonAttrs // {
+            name = "freeciv21-debug";
+            dontStrip = true;
+            dontFixup = true;
+            CFLAGS = "-Og -ggdb";
+            CXXFLAGS = "-Og -ggdb";
+            buildPhase = ''
+              mkdir -p $out/
+              cmake . -B build -G Ninja \
+                -DCMAKE_INSTALL_PREFIX=$out \
+                -DCMAKE_BUILD_TYPE=Debug \
+                -DFREECIV_DOWNLOAD_FONTS=0
+            '';
+        });
+
       in
       {
         packages = {
           freeciv21 = freeciv21;
+          freeciv21-debug = freeciv21-debug;
           default = freeciv21;
         };
       });
