941ace848fbf3724790ea5919ccb35fe8aa37415
Louis Moureaux <m_louis30@yahoo.com>
2025-04-19 06:07:45 +0200

    Add the Qt audio plugin
    We are moving away from SDL for audio to reduce the number of
    dependencies. This introduces a new audio plugin based on Qt Multimedia.
    It has some differences compared to SDL:
    * It doesn't fade audio out when stopping it.
    * WAVE is the only supported format for sound effects. Music supports
      everything supported by the platform.
    
    See #2104.

---
+++ client/audio/audio_qt.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPLv3-or-later
+// SPDX-FileCopyrightText: Freeciv and Freeciv21 Contributors
+// SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
+
+#pragma once
+
+void audio_qt_init();
