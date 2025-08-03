longturn/freeciv21

commit 2408738725242bd2be7b37761dc5f7e9eafa1cbc

Fix a crash when displaying the combat animation on Windows
The game was crashing on Windows when attacking units on a local server. This
was caused by network input being processed in the middle of the animation and
units being deleted. This was caused by anim_delay() processing events.
Strangely enough, this doesn't happen with TCP sockets.

This patch reintroduces a blocking sleep in anim_delay() on Windows. It blocks
the window and might lead to it being unresponsive, but this is better than an
almost guaranteed crash.

A proper fix would include a better framework for animations, which cannot be
done in time for 3.1.

client/views/view_map_common.cpp
@@ -90,12 +90,17 @@ Q_GLOBAL_STATIC(QElapsedTimer, anim_timer);
 
 void anim_delay(int milliseconds)
 {
+#ifdef QT_OS_WIN
+  // Workaround for #2567
+  QThread::msleep(milliseconds);
+#else
   QEventLoop loop;
   QTimer t;
   t.connect(&t, &QTimer::timeout, &loop, &QEventLoop::quit);
   t.start(milliseconds);
   QCoreApplication::processEvents(QEventLoop::AllEvents, 20);
   loop.exec();
+#endif
 }
 
 /**

commit 53e33eccdecc3909fa99740aff7a1b64c2241f7e

Fix macro to select Windows version of anim_delay
Fixes: 2408738725242bd2be7b37761dc5f7e9eafa1cbc.

client/views/view_map_common.cpp
@@ -28,6 +28,7 @@
 #include <QPainter>
 #include <QPixmap>
 #include <QSet>
+#include <QThread> // Windows
 #include <QTimer>
 
 // utility
@@ -90,7 +91,7 @@ Q_GLOBAL_STATIC(QElapsedTimer, anim_timer);
 
 void anim_delay(int milliseconds)
 {
-#ifdef QT_OS_WIN
+#ifdef Q_OS_WIN
   // Workaround for #2567
   QThread::msleep(milliseconds);
 #else