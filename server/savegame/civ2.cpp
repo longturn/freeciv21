faf7b61ef57268265b06c40837312e9c8065f477
Louis Moureaux <m_louis30@yahoo.com>
2025-08-06 01:35:11 +0200

    civ2: read out civ2 version number

--- server/savegame/civ2.cpp
+++ server/savegame/civ2.cpp
@@ -5,6 +5,36 @@
 
 #include <QFile>
 #include <QString>
+#include <QtDebug>
+
+#include <cstdint>
+
+/**
+ * \file civ2.cpp Loads civ2 saves.
+ *
+ * Useful resources:
+ * * https://apolyton.net/forum/civilization-series/civilization-i-and-civilization-ii/130935-civilization-ii-sav-scn-file-format
+ * * http://civgaming.net/mercator/reference/mapstruct.html in the Wayback
+ *   machine
+ * * https://github.com/evyscr/civ2mp2fc/tree/master
+ */
+
+namespace /* anonymous */ {
+const static auto MAGIC = QByteArrayLiteral("CIVILIZE\0\x1A");
+
+/**
+ * Internal Civliization II version numbers.
+ */
+enum civ2_version {
+  CIV2_CLASSIC = 39,         ///< Classic, Conflicts in Civilization
+  CIV2_FANTASTIC = 40,       ///< Fantastic Worlds
+  CIV2_MULTIPLAYER = 44,     ///< Multiplayer v1.3
+  CIV2_TEST_OF_TIME_10 = 49, ///< Test of Time v1.0
+  CIV2_TEST_OF_TIME_11 = 50, ///< Test of Time v1.1
+};
+/// The highest supported version (from above)
+const static auto MAX_VERSION = CIV2_MULTIPLAYER;
+} // namespace
 
 /**
  * Checks if the file at path looks like a civ2 save.
@@ -16,8 +46,7 @@ bool is_civ2_save(const QString &path)
     return false;
   }
 
-  const auto magic = QByteArrayLiteral("CIVILIZE");
-  return input.read(magic.size()) == magic;
+  return input.read(MAGIC.size()) == MAGIC;
 }
 
 /**
@@ -25,6 +54,25 @@ bool is_civ2_save(const QString &path)
  */
 bool load_civ2_save(const QString &path)
 {
+  // Open the file
+  auto file = QFile(path);
+  file.open(QIODevice::ReadOnly);
+  QDataStream bytes(&file);
+  bytes.setByteOrder(QDataStream::LittleEndian);
+
+  // Header
+  bytes.skipRawData(MAGIC.size()); // CIVILIZE 0x00 0x1A
+
+  std::int16_t version;
+  bytes >> version;
+
+  qDebug() << "Loading civ2 file with version" << version;
+  if (version > MAX_VERSION) {
+    qCritical("Unsupported civ2 file version: %d",
+              static_cast<int>(version));
+    return false;
+  }
+
   // Unsupported!
   qCritical("Cannot read civ2 saves!");
   return false;
