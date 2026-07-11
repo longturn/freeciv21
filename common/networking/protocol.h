3fb6b9f06baaa6ec0d6d66e67a150f65b946f02b
Louis Moureaux <m_louis30@yahoo.com>
2026-07-11 21:54:08 +0200

    Only use a map in packet handlers when necessary
    It's only really needed when a key is provided, which is a minority of
    packets. Without a key, the map was adding useless overhead.

--- common/networking/protocol.h
+++ common/networking/protocol.h
@@ -12,6 +12,7 @@
 #include "packets.h"
 
 // std
+#include <optional>
 #include <unordered_map>
 
 /**
@@ -21,12 +22,38 @@
  */
 template <class T> class packet_delta_handler : public packet_handler {
 protected:
-  std::unordered_map<int, T> receive_map, send_map;
+  std::optional<T> last_received, last_sent;
   QBitArray fields;
 
 public:
   packet_delta_handler(int field_count) : fields(field_count) {}
   virtual ~packet_delta_handler() = default;
+
+  virtual void reset() override
+  {
+    last_received = std::nullopt;
+    last_sent = std::nullopt;
+  }
+
+  virtual void reset(int key) override
+  {
+    Q_UNUSED(key);
+    reset();
+  }
+};
+
+/**
+ * Equivalent to \ref packet_delta_handler, but stores packets based on a key
+ * (e.g., the tile or unit id).
+ */
+template <class T> class packet_delta_key_handler : public packet_handler {
+protected:
+  std::unordered_map<int, T> receive_map, send_map;
+  QBitArray fields;
+
+public:
+  packet_delta_key_handler(int field_count) : fields(field_count) {}
+  virtual ~packet_delta_key_handler() = default;
 
   virtual void reset() override
   {
